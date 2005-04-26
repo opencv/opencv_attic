7a197ab5fdd98a9e1be979a7c5f485c1477fc92a
Vadim Pisarevsky <no@email>
2005-04-26 12:52:47 +0000

    fg/bg segmentation sample
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@150 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

---
+++ samples/c/bgfg_segm.cpp
@@ -0,0 +1,46 @@
+#include "cvaux.h"
+#include "highgui.h"
+#include <stdio.h>
+
+//this is a sample for foreground detection functions
+int main(int argc, char** argv)
+{
+    IplImage*       tmp_frame = NULL;
+    CvCapture*      cap = NULL;
+
+    if( argc < 2 )
+    {
+        printf("please specify video file name \n");
+        exit(0);
+    }
+
+    cap = cvCaptureFromFile(argv[1]);
+    tmp_frame = cvQueryFrame(cap);
+    if(!tmp_frame)
+    {
+        printf("bad video \n");
+        exit(0);
+    }
+
+    cvNamedWindow("BG", 1);
+    cvNamedWindow("FG", 1);
+
+    //create BG model
+    CvBGStatModel* bg_model = cvCreateBGStatModel( tmp_frame, CV_BG_MODEL_FGD );
+    
+    for( int fr = 1;tmp_frame; tmp_frame = cvQueryFrame(cap), fr++ )
+    {
+        cvUpdateBGStatModel( tmp_frame, bg_model );
+        cvShowImage("BG", bg_model->background);
+        cvShowImage("FG", bg_model->foreground);
+        int k = cvWaitKey(5);
+        if( k == 27 ) break;
+        printf("frame# %d \r", fr);
+    }
+
+
+    cvReleaseBGStatModel( &bg_model );
+    cvReleaseCapture(&cap);
+
+    return 0;
+}
