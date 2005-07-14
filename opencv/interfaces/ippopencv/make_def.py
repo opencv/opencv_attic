6f8c72b9134a8c3e125fb971558d9c9c76ac3212
Vadim Pisarevsky <no@email>
2005-07-14 09:20:14 +0000

    updated custom IPP dll project files to support both IPP4 & IPP5
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@260 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/interfaces/ippopencv/make_def.py
+++ opencv/interfaces/ippopencv/make_def.py
@@ -1,20 +1,25 @@
 import sys, os, string, re
 tmp_name = "tmp.h"
 
-os.system( "cpp opencvipp_funclist.h > " + tmp_name );
+ipp_ver = 4
+
+if len(sys.argv) == 2 and sys.argv[1] == "5":
+    ipp_ver = 5
+
+os.system( "cpp -DIPP%d opencvipp_funclist.h > %s" % (ipp_ver, tmp_name) );
 f = open( tmp_name, "r" )
 ll = f.readlines()
 f.close()
 os.remove( tmp_name )
 
-f = open( "export.def", "w" )
+f = open( "export%d.def" % (ipp_ver,) , "w" )
 print >>f, \
 """; The file has been generated automatically from opencvipp_funclist.h.
 ; Do not alter it!
    
 EXPORTS"""
 
-fn_regexp = re.compile( r"IPPAPI\s*\(\s*IppStatus,\s*(\w+)" )
+fn_regexp = re.compile( r"IPPAPI\s*\([^,]+,\s*(\w+)" )
 
 for l in ll:
     fn_list = fn_regexp.findall(l)
