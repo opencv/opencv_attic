7f122b08f31d86660d4a39777e535fb7563d6cd1
rstanchak <no@email>
2006-04-28 15:42:44 +0000

    Hacky script for converting OpenCV macros into python functions
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@476 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

---
+++ opencv/utils/extract_macros.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+"""
+This script extracts macros #defines from those OpenCV headers that can't be
+directly parsed by current SWIG versions and must be pre-filtered by
+the C preprocessor (that erases all #defines).  Type information is missing, so
+C code can't be generated for a generic language solution, so only python code is 
+generated
+"""
+
+import sys, re
+
+# macros with illegal python syntax
+EXCLUDE = { 'CV_SWAP':1, 'CV_IABS':1 } 
+
+# force this to be part of cv module
+# otherwise becomes cv.cvmacros
+print "%module cv"
+print "%pythoncode %{"
+for fn in sys.argv[1:]:
+    f = open( fn, "r" )
+    in_define = 0
+    for l in f.xreadlines():
+        # get rid of pointer member accesses
+        (l, n) = re.subn( "->", ".", l)
+        # get rid of casts
+        (l, n) = re.subn( "\((?:Cv\w+|void|uchar|int)\*\)", "", l)
+        # get rid of dereferences
+        (l, n) = re.subn( "\(\*\(", "((", l)
+        m = re.match( r"^#define\s+((?:CV_|IPL_)\w+)\s*\(([_, a-zA-Z0-9]*)\)\s*(.*)", l )
+        if m and not l.endswith( "\\\n" ) and not EXCLUDE.has_key(m.group(1)):
+            print "def %s (%s):" % (m.group(1), m.group(2))
+            print "\treturn %s\n" % (m.group(3))
+    f.close()
+
+print "%}"
