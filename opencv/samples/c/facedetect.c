7f3fc29770ca67739adcb6bc308f449828a3b395
Vadim Pisarevsky <no@email>
2006-05-04 16:50:40 +0000

    minor improvements
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@494 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/samples/c/facedetect.c
+++ opencv/samples/c/facedetect.c
@@ -134,19 +134,25 @@ int main( int argc, char** argv )
 
 void detect_and_draw( IplImage* img )
 {
-    int scale = 1;
-    IplImage* temp = cvCreateImage( cvSize(img->width/scale,img->height/scale), 8, 3 );
+    int scale = 1.3;
+    IplImage* gray = cvCreateImage( cvSize(img->width,img->height), 8, 1 );
+    IplImage* small_img = cvCreateImage( cvSize(img->width/scale,img->height/scale), 8, 1 );
     CvPoint pt1, pt2;
     int i;
 
-    //cvPyrDown( img, temp, CV_GAUSSIAN_5x5 );
+    cvCvtColor( img, gray, CV_BGR2GRAY );
+    cvResize( gray, small_img, CV_INTER_LINEAR );
+    cvEqualizeHist( small_img, small_img );
     cvClearMemStorage( storage );
 
     if( cascade )
     {
-        CvSeq* faces = cvHaarDetectObjects( img, cascade, storage,
-                                            1.1, 2, CV_HAAR_DO_CANNY_PRUNING,
-                                            cvSize(40, 40) );
+        double t = (double)cvGetTickCount();
+        CvSeq* faces = cvHaarDetectObjects( small_img, cascade, storage,
+                                            1.2, 2, 0/*CV_HAAR_DO_CANNY_PRUNING*/,
+                                            cvSize(30, 30) );
+        t = (double)cvGetTickCount() - t;
+        printf( "detection time = %gms\n", t/((double)cvGetTickFrequency()*1000.) );
         for( i = 0; i < (faces ? faces->total : 0); i++ )
         {
             CvRect* r = (CvRect*)cvGetSeqElem( faces, i );
@@ -159,7 +165,8 @@ void detect_and_draw( IplImage* img )
     }
 
     cvShowImage( "result", img );
-    cvReleaseImage( &temp );
+    cvReleaseImage( &gray );
+    cvReleaseImage( &small_img );
 }
 
 #ifdef _EiC
