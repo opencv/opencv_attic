1c882fd7975ee8b5bf2be7a0131e8521f2232f98
rstanchak <no@email>
2006-05-12 21:28:29 +0000

    Clarified comments
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@502 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/utils/extract_macros.py
+++ opencv/utils/extract_macros.py
@@ -2,10 +2,12 @@
 """
 This script extracts macros #defines from those OpenCV headers that can't be
 directly parsed by current SWIG versions and must be pre-filtered by
-the C preprocessor (that erases all #defines).  Type information is missing, so
-C code can't be generated for a generic language solution, so only python code is 
-generated
+the C preprocessor (that erases all #defines).  Type information is missing in the 
+macros, so C code can't be regenerated.  Instead, try to convert C to Python code.
+C macros too complicated to represent in python using regexes are listed in EXCLUDE
 """
+
+# TODO: support multiline macros
 
 import sys, re
 
