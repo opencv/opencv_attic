29b53daae4e1a2023e09f842b1e683a005860281
bornet <no@email>
2006-07-18 10:10:53 +0000

    Remove comments on the generated files.
    This prevent problem when generating with original file like:
    
    #define A_DEFINE_VAR something   /* the comment on the
                                        definition */
    #define ANOTHER_VAR otherthing
    #define ANOTHER_ONE otherone     /* another comment */
    #define LAST_ONE    the_last
    
    Without removing comment, we will have a output like:
    
    #define A_DEFINE_VAR something   /* the comment on the
    #define ANOTHER_VAR otherthing
    #define ANOTHER_ONE otherone     /* another comment */
    #define LAST_ONE    the_last
    
    This cause ANOTHER_VAR and ANOTHER_ONE not recognized, because they are in
    comments in the generated file.
    
    Removing the generated comments will prevent this.
    
    
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@624 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- utils/extract_constants.py
+++ utils/extract_constants.py
@@ -16,7 +16,7 @@ for fn in sys.argv[1:]:
         if re.match (r"^#define\s+CV_MAKETYPE", l):
             in_define = 1
         if in_define:
-            print l[:-1]
+            print l[:l.find ('/*')]
             if not l.endswith( "\\\n" ):
                 in_define = 0
                 print
