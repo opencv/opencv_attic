2b2dfcfded1e712f3f936c370cb27a0f329e0da4
Vadim Pisarevsky <no@email>
2006-08-02 17:09:22 +0000

    added new samples
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@687 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- samples/c/build_all.sh
+++ samples/c/build_all.sh
@@ -3,4 +3,8 @@ for i in *.c; do
     echo "compiling $i"
     g++ `pkg-config --cflags opencv` -o `basename $i .c` $i `pkg-config --libs opencv`;
 done
+for i in *.cpp; do
+    echo "compiling $i"
+    g++ `pkg-config --cflags opencv` -o `basename $i .cpp` $i `pkg-config --libs opencv`;
+done
 
