e247157a12c74c6a9cd85bc8e3dd97f51c8406c9
rstanchak <no@email>
2006-08-10 13:00:48 +0000

    Additional samples and small improvements to build script.
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@730 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

---
+++ samples/c/houghlines.c
@@ -0,0 +1,52 @@
+/* This is a standalone program. Pass an image name as a first parameter of the program.
+   Switch between standard and probabilistic Hough transform by changing "#if 1" to "#if 0" and back */
+#include <cv.h>
+#include <highgui.h>
+#include <math.h>
+
+int main(int argc, char** argv)
+{
+    IplImage* src;
+    if( argc == 2 && (src=cvLoadImage(argv[1], 0))!= 0)
+    {
+        IplImage* dst = cvCreateImage( cvGetSize(src), 8, 1 );
+        IplImage* color_dst = cvCreateImage( cvGetSize(src), 8, 3 );
+        CvMemStorage* storage = cvCreateMemStorage(0);
+        CvSeq* lines = 0;
+        int i;
+        cvCanny( src, dst, 50, 200, 3 );
+        cvCvtColor( dst, color_dst, CV_GRAY2BGR );
+#if 1
+        lines = cvHoughLines2( dst, storage, CV_HOUGH_STANDARD, 1, CV_PI/180, 100, 0, 0 );
+
+        for( i = 0; i < MIN(lines->total,100); i++ )
+        {
+            float* line = (float*)cvGetSeqElem(lines,i);
+            float rho = line[0];
+            float theta = line[1];
+            CvPoint pt1, pt2;
+            double a = cos(theta), b = sin(theta);
+            double x0 = a*rho, y0 = b*rho;
+            pt1.x = cvRound(x0 + 1000*(-b));
+            pt1.y = cvRound(y0 + 1000*(a));
+            pt2.x = cvRound(x0 - 1000*(-b));
+            pt2.y = cvRound(y0 - 1000*(a));
+            cvLine( color_dst, pt1, pt2, CV_RGB(255,0,0), 3, 8, CV_AA );
+        }
+#else
+        lines = cvHoughLines2( dst, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI/180, 50, 50, 10 );
+        for( i = 0; i < lines->total; i++ )
+        {
+            CvPoint* line = (CvPoint*)cvGetSeqElem(lines,i);
+            cvLine( color_dst, line[0], line[1], CV_RGB(255,0,0), 3, 8, CV_AA );
+        }
+#endif
+        cvNamedWindow( "Source", 1 );
+        cvShowImage( "Source", src );
+
+        cvNamedWindow( "Hough", 1 );
+        cvShowImage( "Hough", color_dst );
+
+        cvWaitKey(0);
+    }
+}
