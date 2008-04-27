ccc8335b820dfd92ca1ecc71a9e9d330122462ad
Xavier Delacour <no@email>
2008-04-27 19:17:49 +0000

    Fix more samples: cam-histo.m, capture-cam.m, laplace.m, facedetect.m
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@1154 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/samples/octave/facedetect.m
+++ opencv/samples/octave/facedetect.m
@@ -5,15 +5,21 @@
 ## Original C implementation by:  ?
 ## Python implementation by: Roman Stanchak
 ## Octave implementation by: Xavier Delacour
+addpath("/home/x/opencv2/interfaces/swig/octave");
+source("/home/x/opencv2/interfaces/swig/octave/PKG_ADD_template");
+debug_on_error(true);
+debug_on_warning(true);
+crash_dumps_octave_core (0)
 cv;
 highgui;
 
 
 ## Global Variables
-cascade = [];
-storage = cvCreateMemStorage(0);
-cascade_name = "../../data/haarcascades/haarcascade_frontalface_alt.xml";
-input_name = "../c/lena.jpg";
+global g;
+g.cascade = [];
+g.storage = cvCreateMemStorage(0);
+g.cascade_name = "../../data/haarcascades/haarcascade_frontalface_alt.xml";
+g.input_name = "../c/lena.jpg";
 
 ## Parameters for haar detection
 ## From the API:
@@ -22,34 +28,37 @@ input_name = "../c/lena.jpg";
 ## images the settings are: 
 ## scale_factor=1.2, min_neighbors=2, flags=CV_HAAR_DO_CANNY_PRUNING, 
 ## min_size=<minimum possible face size
-min_size = cvSize(20,20);
-image_scale = 1.3;
-haar_scale = 1.2;
-min_neighbors = 2;
-haar_flags = 0;
+g.min_size = cvSize(20,20);
+g.image_scale = 1.3;
+g.haar_scale = 1.2;
+g.min_neighbors = 2;
+g.haar_flags = 0;
 
 
 function detect_and_draw( img )
+  global g;
+  global cv;
+
   gray = cvCreateImage( cvSize(img.width,img.height), 8, 1 );
-  small_img = cvCreateImage( cvSize( cvRound (img.width/image_scale),
-				    cvRound (img.height/image_scale)), 8, 1 );
-  cvCvtColor( img, gray, CV_BGR2GRAY );
-  cvResize( gray, small_img, CV_INTER_LINEAR );
+  small_img = cvCreateImage( cvSize( cvRound (img.width/g.image_scale),
+				    cvRound (img.height/g.image_scale)), 8, 1 );
+  cvCvtColor( img, gray, cv.CV_BGR2GRAY );
+  cvResize( gray, small_img, cv.CV_INTER_LINEAR );
 
   cvEqualizeHist( small_img, small_img );
   
-  cvClearMemStorage( storage );
+  cvClearMemStorage( g.storage );
 
-  if( cascade )
-    t = cvGetTickCount();
-    faces = cvHaarDetectObjects( small_img, cascade, storage,
-                                haar_scale, min_neighbors, haar_flags, min_size );
-    t = cvGetTickCount() - t;
-    print "detection time = %gms" % (t/(cvGetTickFrequency()*1000.));
-    if (faces)
-      for r in faces,
-        pt1 = cvPoint( int(r.x*image_scale), int(r.y*image_scale));
-        pt2 = cvPoint( int((r.x+r.width)*image_scale), int((r.y+r.height)*image_scale) )
+  if( swig_this(g.cascade) )
+    tic
+    faces = cvHaarDetectObjects( small_img, g.cascade, g.storage,
+                                g.haar_scale, g.min_neighbors, g.haar_flags, g.min_size );
+    toc
+    if (swig_this(faces))
+      for r = CvSeq_map(faces),
+	r = r{1};
+        pt1 = cvPoint( int32(r.x*g.image_scale), int32(r.y*g.image_scale));
+        pt2 = cvPoint( int32((r.x+r.width)*g.image_scale), int32((r.y+r.height)*g.image_scale) );
         cvRectangle( img, pt1, pt2, CV_RGB(255,0,0), 3, 8, 0 );
       endfor
     endif
@@ -59,75 +68,76 @@ function detect_and_draw( img )
 endfunction
 
 
-if (size(argv, 1) > 1)
+if (size(argv, 2) > 0 && (strcmp(argv(){1}, "--help") || strcmp(argv(){1}, "-h")))
+  printf("Usage: facedetect --cascade \"<cascade_path>\" [filename|camera_index]\n");
+  exit(-1);
+endif
 
-  if (argv(1, :).startswith("--cascade="))
-    cascade_name = argv(1, :)(length("--cascade=")) ];
-    if (size(argv, 1) > 2)
-      input_name = argv(2, :);
-
-    elseif (argv(1, :) == "--help" || argv(1, :) == "-h")
-      printf("Usage: facedetect --cascade=\"<cascade_path>\" [filename|camera_index]\n");
-      exit(-1);
-
-    else
-      input_name = argv(1, :);
+if (size(argv, 2) >= 2)
+  if (strcmp(argv(){1},"--cascade"))
+    g.cascade_name = argv(){2};
+    if (size(argv, 2) >= 3)
+      g.input_name = argv(){3};
     endif
   endif
-  
-  ## the OpenCV API says this function is obsolete, but we can't
-  ## cast the output of cvLoad to a HaarClassifierCascade, so use this anyways
-  ## the size parameter is ignored
-  cascade = cvLoadHaarClassifierCascade( cascade_name, cvSize(1,1) );
-  
-  if (!cascade)
-    printf("ERROR: Could not load classifier cascade\n");
-    exit(-1);
-  endif
-  
+elseif (size(argv, 2) == 1)
+  g.input_name = argv(){1};
+endif
 
-  if (input_name.isdigit())
-    capture = cvCreateCameraCapture( int(input_name) );
-  else
-    capture = cvCreateFileCapture( input_name );
-  endif
+## the OpenCV API says this function is obsolete, but we can't
+## cast the output of cvLoad to a HaarClassifierCascade, so use this anyways
+## the size parameter is ignored
+g.cascade = cvLoadHaarClassifierCascade( g.cascade_name, cvSize(1,1) );
 
-  cvNamedWindow( "result", 1 );
+if (!swig_this(g.cascade))
+  printf("ERROR: Could not load classifier cascade\n");
+  exit(-1);
+endif
 
-  if( capture )
-    frame_copy = [];
-    while (true)
-      frame = cvQueryFrame( capture );
-      if( ! frame )
-        break;
-      endif
-      if( not frame_copy )
-        frame_copy = cvCreateImage( cvSize(frame.width,frame.height),
-                                   IPL_DEPTH_8U, \
-				   frame.nChannels );
-      endif
-      if( frame.origin == IPL_ORIGIN_TL )
-        cvCopy( frame, frame_copy );
-      else
-        cvFlip( frame, frame_copy, 0 );
-      endif
-      
-      detect_and_draw( frame_copy );
+g.input_name
 
-      if( cvWaitKey( 10 ) >= 0 )
-        break;
-      endif
-    endwhile
+if (all(isdigit(g.input_name)))
+  capture = cvCreateCameraCapture( sscanf(g.input_name, "%i") );
+else
+  capture = cvCreateFileCapture( g.input_name );
+endif
 
-  else
-    image = cvLoadImage( input_name, 1 );
-    
-    if( image )
-      
-      detect_and_draw( image );
+capture
+
+cvNamedWindow( "result", 1 );
+
+if( swig_this(capture) )
+  frame_copy = [];
+  while (true)
+    frame = cvQueryFrame( capture );
+    if( ! swig_this(frame) )
       cvWaitKey(0);
     endif
-  endif
+    if( !swig_this(frame_copy) )
+      frame_copy = cvCreateImage( cvSize(frame.width,frame.height),
+                                 IPL_DEPTH_8U, frame.nChannels );
+    endif
+    if( frame.origin == IPL_ORIGIN_TL )
+      cvCopy( frame, frame_copy );
+    else
+      cvFlip( frame, frame_copy, 0 );
+    endif
+    
+    detect_and_draw( frame_copy );
+
+    if( cvWaitKey( 10 ) == 27 )
+      break;
+    endif
+  endwhile
+
+else
+  image = cvLoadImage( g.input_name, 1 );
   
-  cvDestroyWindow("result");
+  if( swig_this(image) )
+
+    detect_and_draw( image );
+    cvWaitKey(0);
+  endif
 endif
+
+cvDestroyWindow("result");
--- opencv/samples/octave/laplace.m
+++ opencv/samples/octave/laplace.m
@@ -1,21 +1,26 @@
 #! /usr/bin/env octave
-cv
-highgui
+addpath("/home/x/opencv2/interfaces/swig/octave");
+source("/home/x/opencv2/interfaces/swig/octave/PKG_ADD_template");
+debug_on_error(true);
+debug_on_warning(true);
+crash_dumps_octave_core (0)
+cv;
+highgui;
 
-laplace = []
-colorlaplace = []
-planes = [ [], [], [] ];
-capture = []
+laplace = [];
+colorlaplace = [];
+planes = { [], [], [] };
+capture = [];
 
-if (size(argv, 1)==1)
-  capture = cvCreateCameraCapture( 0 )
-elseif (size(argv, 1)==2 && all(isdigit(argv(1, :))))
-  capture = cvCreateCameraCapture( int32(argv(1, :)) )
-elseif (size(argv, 1)==2)
-  capture = cvCreateFileCapture( argv(1, :) ); 
+if (size(argv, 2)==0)
+  capture = cvCreateCameraCapture( -1 );
+elseif (size(argv, 2)==1 && all(isdigit(argv(){1})))
+  capture = cvCreateCameraCapture( int32(argv(){1}) );
+elseif (size(argv, 2)==1)
+  capture = cvCreateFileCapture( argv(){1} );
 endif
 
-if (! capture)
+if (!swig_this(capture))
   printf("Could not initialize capturing...\n");
   exit(-1)
 endif
@@ -24,13 +29,13 @@ cvNamedWindow( "Laplacian", 1 );
 
 while (true),
   frame = cvQueryFrame( capture );
-  if (!frame)
+  if (!swig_this(frame))
     break
   endif
 
-  if (!laplace)
-    for i=0:len(planes)-1,
-      planes[i] = cvCreateImage( \
+  if (!swig_this(laplace))
+    for i=1:size(planes,2),
+      planes{i} = cvCreateImage( \
 				cvSize(frame.width,frame.height), \
 				8, 1 );
     endfor
@@ -40,18 +45,19 @@ while (true),
 				 8, 3 );
   endif
 
-  cvSplit( frame, planes[0], planes[1], planes[2], [] );
-  for plane in planes,
+  cvSplit( frame, planes{1}, planes{2}, planes{3}, [] );
+  for plane = planes,
+    plane = plane{1};
     cvLaplace( plane, laplace, 3 );
     cvConvertScaleAbs( laplace, plane, 1, 0 );
   endfor
 
-  cvMerge( planes[0], planes[1], planes[2], [], colorlaplace );
-  colorlaplace.origin = frame.origin;
+  cvMerge( planes{1}, planes{2}, planes{3}, [], colorlaplace );
+#  colorlaplace.origin = frame.origin;
 
   cvShowImage("Laplacian", colorlaplace );
 
-  if (cvWaitKey(10) != -1)
+  if (cvWaitKey(10) == 27)
     break;
   endif
 endwhile
