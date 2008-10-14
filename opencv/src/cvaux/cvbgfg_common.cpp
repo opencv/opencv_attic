96becd86343db298b9df1d1767ad6f84f045f023
Vadim Pisarevsky <no@email>
2008-10-14 00:13:35 +0000

    added codebook algorithm for BG/FG segmentation (from OpenCV book)
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@1334 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/src/cvaux/cvbgfg_common.cpp
+++ opencv/src/cvaux/cvbgfg_common.cpp
@@ -66,5 +66,59 @@ CV_IMPL void cvRefineForegroundMaskBySegm( CvSeq* segments, CvBGStatModel*  bg_m
 	cvReleaseImage(&tmp_image);
 }
 
+
+
+CV_IMPL CvSeq*
+cvSegmentFGMask( CvArr* _mask, int poly1Hull0, float perimScale,
+                 CvMemStorage* storage, CvPoint offset )
+{
+    CvMat mstub, *mask = cvGetMat( _mask, &mstub );
+    CvMemStorage* tempStorage = storage ? storage : cvCreateMemStorage();
+    CvSeq *contours, *c;
+    int nContours = 0;
+    CvContourScanner scanner;
+    
+    // clean up raw mask
+    cvMorphologyEx( mask, mask, 0, 0, CV_MOP_OPEN, 1 );
+    cvMorphologyEx( mask, mask, 0, 0, CV_MOP_CLOSE, 1 );
+
+    // find contours around only bigger regions
+    scanner = cvStartFindContours( mask, tempStorage,
+        sizeof(CvContour), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, offset );
+    
+    while( (c = cvFindNextContour( scanner )) != 0 ) 
+    {
+        double len = cvContourPerimeter( c );
+        double q = (mask->rows + mask->cols)/perimScale; // calculate perimeter len threshold
+        if( len < q ) //Get rid of blob if it's perimeter is too small
+            cvSubstituteContour( scanner, 0 );
+        else //Smooth it's edges if it's large enough
+        {
+            CvSeq* newC;
+            if( poly1Hull0 ) //Polygonal approximation of the segmentation 
+                newC = cvApproxPoly( c, sizeof(CvContour), tempStorage, CV_POLY_APPROX_DP, 2, 0 ); 
+            else //Convex Hull of the segmentation
+                newC = cvConvexHull2( c, tempStorage, CV_CLOCKWISE, 1 );
+            cvSubstituteContour( scanner, newC );
+            nContours++;
+        }
+    }
+    contours = cvEndFindContours( &scanner );
+
+    // paint the found regions back into the image
+    cvZero( mask );
+    for( c=contours; c != 0; c = c->h_next ) 
+        cvDrawContours( mask, c, cvScalarAll(255), cvScalarAll(0), -1, CV_FILLED, 8,
+            cvPoint(-offset.x,-offset.y));
+
+    if( tempStorage != storage )
+    {
+        cvReleaseMemStorage( &tempStorage );
+        contours = 0;
+    }
+
+    return contours;
+}
+
 /* End of file. */
 
