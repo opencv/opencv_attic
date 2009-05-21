23dd24cfa8fd468351d21bd69a118822e67c09fc
Vadim Pisarevsky <no@email>
2009-05-21 23:06:36 +0000

    support even kernel size in cv::getGaussianKernel
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@1771 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/src/cv/cvsmooth.cpp
+++ opencv/src/cv/cvsmooth.cpp
@@ -319,15 +319,15 @@ void boxFilter( const Mat& src, Mat& dst, int ddepth,
 Mat getGaussianKernel( int n, double sigma, int ktype )
 {
     const int SMALL_GAUSSIAN_SIZE = 7;
-    static const float small_gaussian_tab[][SMALL_GAUSSIAN_SIZE/2+1] =
+    static const float small_gaussian_tab[][SMALL_GAUSSIAN_SIZE] =
     {
         {1.f},
-        {0.5f, 0.25f},
-        {0.375f, 0.25f, 0.0625f},
-        {0.28125f, 0.21875f, 0.109375f, 0.03125f}
+        {0.25f, 0.5f, 0.25f},
+        {0.0625f, 0.25f, 0.375f, 0.25f, 0.0625f},
+        {0.03125f, 0.109375f, 0.21875f, 0.28125f, 0.21875f, 0.109375f, 0.03125f}
     };
 
-    const float* fixed_kernel = n <= SMALL_GAUSSIAN_SIZE && sigma <= 0 ?
+    const float* fixed_kernel = n % 2 == 1 && n <= SMALL_GAUSSIAN_SIZE && sigma <= 0 ?
         small_gaussian_tab[n>>1] : 0;
 
     CV_Assert( ktype == CV_32F || ktype == CV_64F );
@@ -335,34 +335,34 @@ Mat getGaussianKernel( int n, double sigma, int ktype )
     float* cf = (float*)kernel.data;
     double* cd = (double*)kernel.data;
 
-    double sigmaX = sigma > 0 ? sigma : (n/2 - 1)*0.3 + 0.8;
+    double sigmaX = sigma > 0 ? sigma : ((n-1)*0.5 - 1)*0.3 + 0.8;
     double scale2X = -0.5/(sigmaX*sigmaX);
-
-    double sum = fixed_kernel ? -fixed_kernel[0] : -1.;
+    double sum = 0;
 
     int i;
-    for( i = 0; i <= n/2; i++ )
+    for( i = 0; i < n; i++ )
     {
-        double t = fixed_kernel ? (double)fixed_kernel[i] : std::exp(scale2X*i*i);
+        double x = i - (n-1)*0.5;
+        double t = fixed_kernel ? (double)fixed_kernel[i] : std::exp(scale2X*x*x);
         if( ktype == CV_32F )
         {
-            cf[n/2+i] = (float)t;
-            sum += cf[n/2+i]*2;
+            cf[i] = (float)t;
+            sum += cf[i];
         }
         else
         {
-            cd[n/2+i] = t;
-            sum += cd[n/2+i]*2;
+            cd[i] = t;
+            sum += cd[i];
         }
     }
 
     sum = 1./sum;
-    for( i = 0; i <= n/2; i++ )
+    for( i = 0; i < n; i++ )
     {
         if( ktype == CV_32F )
-            cf[n/2+i] = cf[n/2-i] = (float)(cf[n/2+i]*sum);
+            cf[i] = (float)(cf[i]*sum);
         else
-            cd[n/2+i] = cd[n/2-i] = cd[n/2+i]*sum;
+            cd[i] *= sum;
     }
 
     return kernel;
