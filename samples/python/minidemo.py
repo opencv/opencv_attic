792cebc16435886de432b147840fc241636a9d5e
Martin Baeum <no@email>
2009-06-02 15:14:46 +0000

    - fix extract_aliases.py: record alias if define-line ends with whitespace - update python demo applications: (thanks to Tim Faudot for the patch)   - make demos run with most current version of the wrappers   - clean up: remove many unnecessary semicolons
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@1806 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- samples/python/minidemo.py
+++ samples/python/minidemo.py
@@ -1,17 +1,14 @@
+#! /usr/bin/env python
+
 import opencv
 from opencv import highgui
 
-highgui.cvNamedWindow("win", highgui.CV_WINDOW_AUTOSIZE)
-cap = highgui.cvCaptureFromFile("/home/asbach/Source/ObjectDetection/older/avi/table.avi")
+cap = highgui.cvCreateFileCapture("../c/tree.avi")
 img = highgui.cvQueryFrame(cap)
-
-print img
 print "Got frame of dimensions (", img.width, " x ", img.height, " )"
 
+highgui.cvNamedWindow("win", highgui.CV_WINDOW_AUTOSIZE)
 highgui.cvShowImage("win", img)
 highgui.cvMoveWindow("win", 200, 200)
 highgui.cvWaitKey(0)
-
-pilimg = opencv.Ipl2PIL(img)
-print pilimg
 
