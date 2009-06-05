b08d79652bd7cb3fc88df9b4f79f66857c3ee5a0
James Bowman <no@email>
2009-06-05 02:26:16 +0000

    Plastex
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@1815 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

---
+++ doc2/plastex/myopencv.py
@@ -0,0 +1,38 @@
+
+from plasTeX import Base
+from plasTeX.Base.LaTeX.Verbatim import verbatim
+
+class includegraphics(Base.Command):
+  args = '[size] file'
+  def invoke(self, tex):
+    Base.Command.invoke(self, tex)
+
+class cvfunc(Base.Command):
+  args = 'title'
+  def invoke(self, tex):
+    Base.Command.invoke(self, tex)
+
+class cross(Base.Command):
+  args = 'name'
+  def invoke(self, tex):
+    Base.Command.invoke(self, tex)
+
+class label(Base.Command):
+  args = 'name'
+  def invoke(self, tex):
+    Base.Command.invoke(self, tex)
+
+class cvarg(Base.Command):
+  args = 'item def'
+  def invoke(self, tex):
+    Base.Command.invoke(self, tex)
+
+class cvexp(Base.Command):
+  args = 'c cpp py'
+  def invoke(self, tex):
+    Base.Command.invoke(self, tex)
+
+class lstlisting(verbatim):
+  def parse(self, tex):
+    verbatim.parse(self, tex)
+    return self.attributes
