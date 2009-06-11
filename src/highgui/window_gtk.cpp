84a2643a3816a815f1495957e2f703bd3b1688d4
Vadim Pisarevsky <no@email>
2009-06-11 21:20:34 +0000

    removed a few garbage characters
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@1828 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- src/highgui/window_gtk.cpp
+++ src/highgui/window_gtk.cpp
@@ -844,7 +844,7 @@ icvFindTrackbarByName( const CvWindow* window, const char* name )
 
 static int
 icvCreateTrackbar( const char* trackbar_name, const char* window_name,
-                   int* val, int count, CvTrackbarCallback on_notify,
+                   int* val, int count, CvTrackbarCallback on_notify,
                    CvTrackbarCallback2 on_notify2, void* userdata )
 {
     int result = 0;
@@ -946,7 +946,7 @@ cvCreateTrackbar( const char* trackbar_name, const char* window_name,
 
 CV_IMPL int
 cvCreateTrackbar2( const char* trackbar_name, const char* window_name,
-                   int* val, int count, CvTrackbarCallback2 on_notify2,
+                   int* val, int count, CvTrackbarCallback2 on_notify2,
                    void* userdata )
 {
     return icvCreateTrackbar(trackbar_name, window_name, val, count,
