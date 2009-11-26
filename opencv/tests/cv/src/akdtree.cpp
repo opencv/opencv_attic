32e3365f7694649a8789e8d91c28fd8755ef8296
Maria Dimashova <no@email>
2009-11-26 15:36:27 +0000

    added cpp_kdtree test
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2330 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/tests/cv/src/akdtree.cpp
+++ opencv/tests/cv/src/akdtree.cpp
@@ -8,6 +8,8 @@
 #include <vector>
 #include <iostream>
 
+using namespace cv;
+
 class CV_KdtreeTest : public CvTest {
 public:
   CV_KdtreeTest();
@@ -70,5 +72,65 @@ void CV_KdtreeTest::run( int /*start_from*/ ) {
   cvReleaseFeatureTree(tr);
 }
 
-
 CV_KdtreeTest kdtree_test;
+
+
+
+// ----------------------------- test for cv::KDTree -----------------------
+class KDTreeTest : public CvTest {
+public:
+    KDTreeTest();
+    ~KDTreeTest();
+protected:
+    virtual void run( int start_from );
+};
+
+KDTreeTest::KDTreeTest()
+: CvTest( "cpp_kdtree", "cv::KDTree funcs" )
+{
+}
+KDTreeTest::~KDTreeTest() 
+{
+}
+
+void KDTreeTest::run( int /*start_from*/ ) {
+    int dims = 64;
+    int featuresCount = 2000;
+    int K = 1; // * should also test 2nd nn etc.?
+    int emax = 2000;
+    float noise = 0.2f;
+    int pointsCount = 1000;
+
+    RNG rng;
+    Mat desc( featuresCount, dims, CV_32FC1 );
+    rng.fill( desc, RNG::UNIFORM, Scalar(0.0f), Scalar(1.0f) );
+
+    KDTree tr( desc );
+    Mat pts( pointsCount, dims, CV_32FC1 );
+    Mat results( pointsCount, K, CV_32SC1 );
+
+    std::vector<int> fmap( pointsCount );
+    for( int pi = 0; pi < pointsCount; pi++ )
+    {
+        int fi = rng.next() % featuresCount;
+        fmap[pi] = fi;
+        for( int d = 0; d < dims; d++ )
+            pts.at<float>(pi, d) = desc.at<float>(fi, d) + rng.uniform(0.0f, 1.0f) * noise;
+        tr.findNearest( pts.ptr<float>(pi), K, emax, results.ptr<int>(pi) );
+    }
+
+    int correctMatches = 0;
+    for( int pi = 0; pi < pointsCount; pi++ )
+    {
+        if( fmap[pi] == results.at<int>(pi, 0) )
+            correctMatches++;
+    }
+
+    double correctPerc = correctMatches / (double)pointsCount;
+    ts->printf( CvTS::LOG, "correct_perc = %d\n", correctPerc );
+    if (correctPerc < .8)
+        ts->set_failed_test_info(CvTS::FAIL_INVALID_OUTPUT);
+}
+
+KDTreeTest cpp_kdtree_test;
+
