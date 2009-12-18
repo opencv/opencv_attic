2c00aa75dae472213d0ec4b85d3d4eef14098d30
Maria Dimashova <no@email>
2009-12-18 12:55:46 +0000

    fixed cvEllipseBox
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2458 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/include/opencv/cxcore.h
+++ opencv/include/opencv/cxcore.h
@@ -1322,8 +1322,8 @@ CV_INLINE  void  cvEllipseBox( CvArr* img, CvBox2D box, CvScalar color,
                                int line_type CV_DEFAULT(8), int shift CV_DEFAULT(0) )
 {
     CvSize axes;
-    axes.width = cvRound(box.size.height*0.5);
-    axes.height = cvRound(box.size.width*0.5);
+    axes.width = cvRound(box.size.width*0.5);
+    axes.height = cvRound(box.size.height*0.5);
 
     cvEllipse( img, cvPointFrom32f( box.center ), axes, box.angle,
                0, 360, color, thickness, line_type, shift );
