fc6af0797ae977d742fb8d67645f67d1ee3fc2e3
Anatoly Baksheev <no@email>
2009-12-28 15:13:10 +0000

    chessboard test patterns generator
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@2497 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

---
+++ tests/cv/src/cvchessboardgenerator.h
@@ -0,0 +1,36 @@
+#ifndef CV_CHESSBOARDGENERATOR_H143KJTVYM389YTNHKFDHJ89NYVMO3VLMEJNTBGUEIYVCM203P
+#define CV_CHESSBOARDGENERATOR_H143KJTVYM389YTNHKFDHJ89NYVMO3VLMEJNTBGUEIYVCM203P
+
+#include "cv.h"
+
+namespace cv
+{
+
+class ChessBoardGenerator
+{
+public:
+    double sensorWidth; 
+    double sensorHeight;     
+    size_t squareEdgePointsNum;
+    double min_cos;
+    mutable double cov;
+    Size patternSize;
+    int rendererResolutionMultiplier;
+
+    ChessBoardGenerator(const Size& patternSize = Size(8, 6));
+    Mat operator()(const Mat& bg, const Mat& camMat, const Mat& distCoeffs) const;    
+    Mat operator ()(const Mat& bg, const Mat& camMat, const Mat& distCoeffs, const Size2f& squareSize) const;  
+    Mat operator ()(const Mat& bg, const Mat& camMat, const Mat& distCoeffs, const Size2f& squareSize, const Point3f& pos) const;
+private:
+    void generateEdge(const Point3f& p1, const Point3f& p2, vector<Point3f>& out) const;
+    Mat generageChessBoard(const Mat& bg, const Mat& camMat, const Mat& distCoeffs, const Point3f& zero, const Point3f& pb1, const Point3f& pb2, float sqWidth, float sqHeight, const vector<Point3f>& whole) const;
+    void generateBasis(Point3f& pb1, Point3f& pb2) const;  
+    Point3f generateChessBoardCenter(const Mat& camMat, const Size& imgSize) const;
+
+    Mat rvec, tvec;
+};
+
+};
+
+
+#endif
