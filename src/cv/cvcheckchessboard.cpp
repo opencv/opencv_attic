a7d111a59848bd24b236aca9f27b64859a6ef34c
Victor Erukhimov <no@email>
2009-12-30 21:11:38 +0000

    Improved detection for highly distorted chessboards
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@2522 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- src/cv/cvcheckchessboard.cpp
+++ src/cv/cvcheckchessboard.cpp
@@ -50,29 +50,43 @@
 #include "highgui.h"
 #endif
 
-void icvGetQuadrangleHypotheses(CvSeq* contours, std::vector<float>& quads)
+void icvGetQuadrangleHypotheses(CvSeq* contours, std::vector<std::pair<float, int> >& quads, int class_id)
 {
-    const float min_aspect_ratio = 0.7f;
-    const float max_aspect_ratio = 1.3f;
+    const float min_aspect_ratio = 0.3f;
+    const float max_aspect_ratio = 3.0f;
     const float min_box_size = 10.0f;
     
     for(CvSeq* seq = contours; seq != NULL; seq = seq->h_next)
     {
         CvBox2D box = cvMinAreaRect2(seq);
+        float box_size = MAX(box.size.width, box.size.height);
+        if(box_size < min_box_size)
+        {
+            continue;
+        }
+
         float aspect_ratio = box.size.width/MAX(box.size.height, 1);
         if(aspect_ratio < min_aspect_ratio || aspect_ratio > max_aspect_ratio)
         {
             continue;
         }
         
-        float box_size = MAX(box.size.width, box.size.height);
-        if(box_size < min_box_size)
-        {
-            continue;
-        }
-        
-        quads.push_back(box_size);
+        quads.push_back(std::pair<float, int>(box_size, class_id));
     }
+}
+
+void countClasses(const std::vector<std::pair<float, int> >& pairs, size_t idx1, size_t idx2, std::vector<int>& counts)
+{
+    counts.assign(2, 0);
+    for(size_t i = idx1; i != idx2; i++)
+    {
+        counts[pairs[i].second]++;
+    }
+}
+
+bool less_pred(const std::pair<float, int>& p1, const std::pair<float, int>& p2)
+{
+    return p1.first < p2.first;
 }
 
 // does a fast check if a chessboard is in the input image. This is a workaround to 
@@ -96,8 +110,9 @@ int cvCheckChessboard(IplImage* src, CvSize size)
     }
     
     const int erosion_count = 1;
-    const double black_level = 60;
-    const double white_level = 160;
+    const double black_level = 20;
+    const double white_level = 130;
+    const double black_white_gap = 70;
     
 #if defined(DEBUG_WINDOWS)
     cvNamedWindow("1", 1);
@@ -117,7 +132,7 @@ int cvCheckChessboard(IplImage* src, CvSize size)
     int result = 0;
     for(float thresh_level = black_level; thresh_level < white_level && !result; thresh_level += 20.0f)
     {
-        cvThreshold(white, thresh, thresh_level, 255, CV_THRESH_BINARY);
+        cvThreshold(white, thresh, thresh_level + black_white_gap, 255, CV_THRESH_BINARY);
         
 #if defined(DEBUG_WINDOWS)
         cvShowImage("1", thresh);
@@ -125,9 +140,9 @@ int cvCheckChessboard(IplImage* src, CvSize size)
 #endif //DEBUG_WINDOWS
         
         CvSeq* first = 0;
-        std::vector<float> quads;
+        std::vector<std::pair<float, int> > quads;
         cvFindContours(thresh, storage, &first, sizeof(CvContour), CV_RETR_CCOMP);        
-        icvGetQuadrangleHypotheses(first, quads);
+        icvGetQuadrangleHypotheses(first, quads, 1);
         
         cvThreshold(black, thresh, thresh_level, 255, CV_THRESH_BINARY_INV);
         
@@ -137,21 +152,21 @@ int cvCheckChessboard(IplImage* src, CvSize size)
 #endif //DEBUG_WINDOWS
         
         cvFindContours(thresh, storage, &first, sizeof(CvContour), CV_RETR_CCOMP);
-        icvGetQuadrangleHypotheses(first, quads);
+        icvGetQuadrangleHypotheses(first, quads, 0);
         
-        std::sort(quads.begin(), quads.end());
+        const size_t min_quads_count = size.width*size.height/2;
+        std::sort(quads.begin(), quads.end(), less_pred);
         
         // now check if there are many hypotheses with similar sizes
         // do this by floodfill-style algorithm
-        const size_t min_quads_count = size.width*size.height/2;
-        const float size_rel_dev = 0.3f;
+        const float size_rel_dev = 0.4f;
         
         for(size_t i = 0; i < quads.size(); i++)
         {
             size_t j = i + 1;
             for(; j < quads.size(); j++)
             {
-                if(quads[j]/quads[i] > 1.0f + size_rel_dev)
+                if(quads[j].first/quads[i].first > 1.0f + size_rel_dev)
                 {
                     break;
                 }
@@ -159,6 +174,16 @@ int cvCheckChessboard(IplImage* src, CvSize size)
             
             if(j + 1 > min_quads_count + i)
             {
+                // check the number of black and white squares
+                std::vector<int> counts;
+                countClasses(quads, i, j, counts);
+                const int black_count = ceil(size.width/2)*ceil(size.height/2);
+                const int white_count = floor(size.width/2)*floor(size.height/2);
+                if(counts[0] < black_count*0.75 ||
+                   counts[1] < white_count*0.75)
+                {
+                    continue;
+                }
                 result = 1;
                 break;
             }
