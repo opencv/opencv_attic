e3cfd206297336de53e5930e500d961e85d0aaf0
Vadim Pisarevsky <no@email>
2010-01-19 14:15:41 +0000

    make jconfig.h compatible with both Windows and Linux builds
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@2582 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- 3rdparty/include/jconfig.h
+++ 3rdparty/include/jconfig.h
@@ -1,30 +1,45 @@
-/* jconfig.vc --- jconfig.h for Microsoft Visual C++ on Windows 95 or NT. */
+/* jconfig.h.  Generated automatically by configure.  */
+/* jconfig.cfg --- source file edited by configure script */
 /* see jconfig.doc for explanations */
 
-#define HAVE_PROTOTYPES
-#define HAVE_UNSIGNED_CHAR
-#define HAVE_UNSIGNED_SHORT
-/* #define void char */
-/* #define const */
+#define HAVE_PROTOTYPES 
+#define HAVE_UNSIGNED_CHAR 
+#define HAVE_UNSIGNED_SHORT 
+/*#undef void*/
+/*#undef const*/
 #undef CHAR_IS_UNSIGNED
+
+#if !defined WIN32 && !defined _WIN32
 #define HAVE_STDDEF_H
-#define HAVE_STDLIB_H
+#define HAVE_STDLIB_H 
+#endif
+ 
 #undef NEED_BSD_STRINGS
 #undef NEED_SYS_TYPES_H
-#undef NEED_FAR_POINTERS	/* we presume a 32-bit flat memory model */
+#undef NEED_FAR_POINTERS
 #undef NEED_SHORT_EXTERNAL_NAMES
+/* Define this if you get warnings about undefined structures. */
 #undef INCOMPLETE_TYPES_BROKEN
 
+#if defined WIN32 || defined _WIN32
 /* Define "boolean" as unsigned char, not int, per Windows custom */
 #ifndef __RPCNDR_H__		/* don't conflict if rpcndr.h already read */
 typedef unsigned char boolean;
 #endif
 #define HAVE_BOOLEAN		/* prevent jmorecfg.h from redefining it */
-
+#endif
 
 #ifdef JPEG_INTERNALS
 
 #undef RIGHT_SHIFT_IS_UNSIGNED
+
+/* These are for configuring the JPEG memory manager. */
+#define DEFAULT_MAX_MEM 1073741824
+
+#if !defined WIN32 && !defined _WIN32
+#define INLINE __inline__
+#undef NO_MKTEMP
+#endif
 
 #endif /* JPEG_INTERNALS */
 
@@ -36,10 +51,17 @@ typedef unsigned char boolean;
 #undef RLE_SUPPORTED		/* Utah RLE image file format */
 #define TARGA_SUPPORTED		/* Targa image file format */
 
+#if defined WIN32 || defined _WIN32
 #define TWO_FILE_COMMANDLINE	/* optional */
 #define USE_SETMODE		/* Microsoft has setmode() */
+#else
+#undef TWO_FILE_COMMANDLINE
+#endif
+
 #undef NEED_SIGNAL_CATCHER
 #undef DONT_USE_B_MODE
-#undef PROGRESS_REPORT		/* optional */
+
+/* Define this if you want percent-done progress reports from cjpeg/djpeg. */
+#undef PROGRESS_REPORT
 
 #endif /* JPEG_CJPEG_DJPEG */
