384fe7518739977acb30c345534a0e79ce1948a7
Anatoly Baksheev <no@email>
2010-02-01 16:34:23 +0000

    throw exception instead of crash causing for win32
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2653 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/tests/cxts/cxts.cpp
+++ opencv/tests/cxts/cxts.cpp
@@ -1298,9 +1298,9 @@ int CvTS::run( int argc, char** argv )
     // 0. reset all the parameters, reorder tests
     clear();
 
-#if defined WIN32 || defined _WIN32
+/*#if defined WIN32 || defined _WIN32
 	cv::setBreakOnError(true);
-#endif
+#endif*/
 
     for( test = get_first_test(), i = 0; test != 0; test = test->get_next(), i++ )
         all_tests.push(test);
