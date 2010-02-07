9d73ad8e2e7c327ec7aa22132dd25b8d8fba467e
Vadim Pisarevsky <no@email>
2010-02-07 08:51:16 +0000

    uncommented rectangle and cvRectangleR drawing functions
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2673 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/include/opencv/cxcore.hpp
+++ opencv/include/opencv/cxcore.hpp
@@ -1147,9 +1147,9 @@ CV_EXPORTS void rectangle(Mat& img, Point pt1, Point pt2,
                           const Scalar& color, int thickness=1,
                           int lineType=8, int shift=0);
 
-//CV_EXPORTS void rectangle(Mat& img, Rect &rec,
-//                          const Scalar& color, int thickness=1,
-//                          int lineType=8, int shift=0);
+CV_EXPORTS void rectangle(Mat& img, Rect rec,
+                          const Scalar& color, int thickness=1,
+                          int lineType=8, int shift=0);
 
 CV_EXPORTS void circle(Mat& img, Point center, int radius,
                        const Scalar& color, int thickness=1,
