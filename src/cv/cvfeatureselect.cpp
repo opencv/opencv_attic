faec2ba2aa2900878aaa9efffd4ad546420dc5a2
Vadim Pisarevsky <no@email>
2010-02-10 23:17:11 +0000

    improved goodfeaturestotrack performance (thanks to Nghia Ho)
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@2682 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- src/cv/cvfeatureselect.cpp
+++ src/cv/cvfeatureselect.cpp
@@ -39,6 +39,8 @@
 //
 //M*/
 #include "_cv.h"
+#include <cstdio>
+#include <vector>
 
 namespace cv
 {
@@ -91,10 +93,97 @@ void goodFeaturesToTrack( const Mat& image, vector<Point2f>& corners,
     sort( tmpCorners, greaterThanPtr<float>() );
     corners.clear();
     size_t i, j, total = tmpCorners.size(), ncorners = 0;
-    
-    minDistance *= minDistance;
 
-    // select the strongest features
+    if(minDistance >= 1)
+    {
+         // Partition the image into larger grids
+        int w = image.cols;
+        int h = image.rows;
+
+        const int cell_size = (int)(minDistance);
+        const int grid_width = (float)w / cell_size + 0.5; // round up
+        const int grid_height = (float)h / cell_size + 0.5; // round up
+
+        std::vector <  std::vector <Point2f> > grid(grid_width*grid_height);
+
+        minDistance *= minDistance;
+
+        for( i = 0; i < total; i++ )
+        {
+            int ofs = (int)((const uchar*)tmpCorners[i] - eig.data);
+            int y = (int)(ofs / eig.step);
+            int x = (int)((ofs - y*eig.step)/sizeof(float));
+
+	        bool good = true;
+
+            int x_cell = x / cell_size;
+            int y_cell = y / cell_size;
+
+            int x1 = x_cell - 1;
+            int y1 = y_cell - 1;
+            int x2 = x_cell + 1;
+            int y2 = y_cell + 1;
+
+            // boundary check
+            x1 = std::max(0, x1);
+            y1 = std::max(0, y1);
+            x2 = std::min(grid_width-1, x2);
+            y2 = std::min(grid_height-1, y2);
+
+            for( int yy = y1; yy <= y2; yy++ )
+            {
+                for( int xx = x1; xx <= x2; xx++ )
+                {   
+                    vector <Point2f> &m = grid[yy*grid_width + xx];
+
+                    if( m.size() )
+                    {
+                        for(j = 0; j < m.size(); j++)
+                        {
+                            float dx = x - m[j].x;
+                            float dy = y - m[j].y;
+
+                            if( dx*dx + dy*dy < minDistance )
+                            {
+                                good = false;
+                                goto break_out;
+                            }
+                        }
+                    }                
+                }
+            }
+
+            break_out:
+
+            if(good)
+            {
+                // printf("%d: %d %d -> %d %d, %d, %d -- %d %d %d %d, %d %d, c=%d\n",
+                //    i,x, y, x_cell, y_cell, (int)minDistance, cell_size,x1,y1,x2,y2, grid_width,grid_height,c);
+                grid[y_cell*grid_width + x_cell].push_back(Point2f((float)x, (float)y));
+
+                corners.push_back(Point2f((float)x, (float)y));
+                ++ncorners;
+
+                if( maxCorners > 0 && (int)ncorners == maxCorners )
+                    break;
+            }
+        }
+    }
+    else
+    {
+        for( i = 0; i < total; i++ )
+        {
+            int ofs = (int)((const uchar*)tmpCorners[i] - eig.data);
+            int y = (int)(ofs / eig.step);
+            int x = (int)((ofs - y*eig.step)/sizeof(float));
+
+            corners.push_back(Point2f((float)x, (float)y));
+            ++ncorners;
+            if( maxCorners > 0 && (int)ncorners == maxCorners )
+                break;
+        }
+    }
+/*
     for( i = 0; i < total; i++ )
     {
         int ofs = (int)((const uchar*)tmpCorners[i] - eig.data);
@@ -119,6 +208,7 @@ void goodFeaturesToTrack( const Mat& image, vector<Point2f>& corners,
         if( maxCorners > 0 && (int)ncorners == maxCorners )
             break;
     }
+*/
 }
     
 void write(FileStorage& fs, const string& objname, const vector<KeyPoint>& keypoints)
