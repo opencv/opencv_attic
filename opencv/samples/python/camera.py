370a71938b436952de16acf1f851198989fbc9dc
James Bowman <no@email>
2010-03-04 22:11:28 +0000

    Simple camera demo
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2745 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

---
+++ opencv/samples/python/camera.py
@@ -0,0 +1,11 @@
+import cv
+
+cv.NamedWindow("camera", 1)
+
+capture = cv.CaptureFromCAM(0)
+
+while True:
+    img = cv.QueryFrame(capture)
+    cv.ShowImage("camera", img)
+    if cv.WaitKey(10) == 27:
+        break
