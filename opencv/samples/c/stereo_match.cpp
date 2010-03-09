ef3691fd17d2ee660912a926ec38253c236e6779
Vadim Pisarevsky <no@email>
2010-03-09 00:32:17 +0000

    extended stereo_match.cpp. Now it can run SGBM too
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2759 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/samples/c/stereo_match.cpp
+++ opencv/samples/c/stereo_match.cpp
@@ -9,240 +9,239 @@
 
 #include <cv.h>
 #include <highgui.h>
-#include <cstdio>
+#include <stdio.h>
 
-using namespace std;
+using namespace cv;
 
-void saveXYZ(const char* filename, CvMat* mat)
+void saveXYZ(const char* filename, const Mat& mat)
 {
     const double max_z = 1.0e4;
     FILE* fp = fopen(filename, "wt");
-    for(int y = 0; y < mat->rows; y++)
+    for(int y = 0; y < mat.rows; y++)
     {
-        for(int x = 0; x < mat->cols; x++)
+        for(int x = 0; x < mat.cols; x++)
         {
-            CvScalar point = cvGet2D(mat, y, x);
-            if(fabs(point.val[2] - max_z) < FLT_EPSILON || fabs(point.val[2]) > max_z) continue;
-            fprintf(fp, "%f %f %f\n", point.val[0], point.val[1], point.val[2]);
+            Vec3f point = mat.at<Vec3f>(y, x);
+            if(fabs(point[2] - max_z) < FLT_EPSILON || fabs(point[2]) > max_z) continue;
+            fprintf(fp, "%f %f %f\n", point[0], point[1], point[2]);
         }
     }
     fclose(fp);
-    
-    cvSave("temp_xyz.yml", mat);
 }
 
-void reproject(CvMat* Q, CvMat* disp)
+void print_help()
 {
-    float xp1 = -cvGet2D(Q, 0, 3).val[0];
-    float yp1 = -cvGet2D(Q, 1, 3).val[0]; 
-    float f = cvGet2D(Q, 2, 3).val[0];
-    float invs = cvGet2D(Q, 3, 2).val[0];
-    
-    double min_disp, max_disp;
-    cvMinMaxLoc(disp, &min_disp, &max_disp, 0, 0);
-    
-    FILE* fp = fopen("xyz_temp.off", "wt");
-    int count = 0;
-    for(int y = 0; y < disp->rows; y++)
-    {
-        for(int x = 0; x < disp->cols; x++)
-        {
-            float d = cvGet2D(disp, y, x).val[0];
-            if(fabs(d - min_disp) < FLT_EPSILON) 
-            {
-                count++;
-                continue;
-            }
-            
-            float X = (x - xp1)/(d*invs);
-            float Y = (y - yp1)/(d*invs);
-            float Z = f/(d*invs);
-            
-            fprintf(fp, "%f %f %f\n", X, Y, Z);
-        }
-    }
-    fclose(fp);
-    printf("Filtered out %d points\n", count);
-}
-
-inline const char* getarg(int argc, char* const* argv, int idx)
-{
-    return argc > idx ? argv[idx] : NULL;
+    printf("Usage: stereo_match <left_image> <right_image> [--algorithm=bm|sgbm|hh] [--blocksize=<block_size>]\n"
+           "[--max-disparity=<max_disparity>] [-i <intrinsic_filename>] [-e <extrinsic_filename>]\n"
+           "[--no-display] [-o <disparity_image>] [-p <point_cloud_file>]\n");
 }
 
 int main(int argc, char** argv)
 {
-    if(argc < 5)
+    const char* algorithm_opt = "--algorithm=";
+    const char* maxdisp_opt = "--max-disparity=";
+    const char* blocksize_opt = "--blocksize=";
+    const char* nodisplay_opt = "--no-display=";
+    
+    if(argc < 3)
     {
-        printf("Usage: stereo_match <img1> <img2> <intrinsic_filename> <extrinsic_filename> [<disparity>] [<point_cloud>]\n");
+        print_help();
         return 0;
     }
-    const char* img1_filename = argv[1];
-    const char* img2_filename = argv[2];
-    const char* intrinsic_filename = argv[3];
-    const char* extrinsic_filename = argv[4];
-    const char* disparity_filename = getarg(argc, argv, 5);
-    const char* point_cloud_filename = getarg(argc, argv, 6);
+    const char* img1_filename = 0;
+    const char* img2_filename = 0;
+    const char* intrinsic_filename = 0;
+    const char* extrinsic_filename = 0;
+    const char* disparity_filename = 0;
+    const char* point_cloud_filename = 0;
+    
+    enum { STEREO_BM=0, STEREO_SGBM=1, STEREO_HH=2 };
+    int alg = STEREO_SGBM;
+    int SADWindowSize = 0, numberOfDisparities = 0;
+    bool no_display = false;
+    
+    StereoBM bm;
+    StereoSGBM sgbm;
+    
+    for( int i = 1; i < argc; i++ )
+    {
+        if( argv[i][0] != '-' )
+        {
+            if( !img1_filename )
+                img1_filename = argv[i];
+            else
+                img2_filename = argv[i];
+        }
+        else if( strncmp(argv[i], algorithm_opt, strlen(algorithm_opt)) == 0 )
+        {
+            char* _alg = argv[i] + strlen(algorithm_opt);
+            alg = strcmp(_alg, "bm") == 0 ? STEREO_BM :
+                  strcmp(_alg, "sgbm") == 0 ? STEREO_SGBM :
+                  strcmp(_alg, "hh") == 0 ? STEREO_HH : -1;
+            if( alg < 0 )
+            {
+                printf("Command-line parameter error: Unknown stereo algorithm\n\n");
+                print_help();
+                return -1;
+            }
+        }
+        else if( strncmp(argv[i], maxdisp_opt, strlen(maxdisp_opt)) == 0 )
+        {
+            if( sscanf( argv[i] + strlen(maxdisp_opt), "%d", &numberOfDisparities ) != 1 ||
+                numberOfDisparities < 1 || numberOfDisparities % 16 != 0 )
+            {
+                printf("Command-line parameter error: The max disparity (--maxdisparity=<...>) must be a positive integer divisible by 16\n");
+                print_help();
+                return -1;
+            }
+        }
+        else if( strncmp(argv[i], blocksize_opt, strlen(blocksize_opt)) == 0 )
+        {
+            if( sscanf( argv[i] + strlen(blocksize_opt), "%d", &SADWindowSize ) != 1 ||
+                SADWindowSize < 1 || SADWindowSize % 2 != 1 )
+            {
+                printf("Command-line parameter error: The block size (--blocksize=<...>) must be a positive odd number\n");
+                return -1;
+            }
+        }
+        else if( strcmp(argv[i], nodisplay_opt) == 0 )
+            no_display = true;
+        else if( strcmp(argv[i], "-i" ) == 0 )
+            intrinsic_filename = argv[++i];
+        else if( strcmp(argv[i], "-e" ) == 0 )
+            extrinsic_filename = argv[++i];
+        else if( strcmp(argv[i], "-o" ) == 0 )
+            disparity_filename = argv[++i];
+        else if( strcmp(argv[i], "-p" ) == 0 )
+            point_cloud_filename = argv[++i];
+        else
+        {
+            printf("Command-line parameter error: unknown option %s\n", argv[i]);
+            return -1;
+        }
+    }
+    
+    if( !img1_filename || !img2_filename )
+    {
+        printf("Command-line parameter error: both left and right images must be specified\n");
+        return -1;
+    }
+    
+    if( (intrinsic_filename != 0) ^ (extrinsic_filename) != 0 )
+    {
+        printf("Command-line parameter error: either both intrinsic and extrinsic parameters must be specified, or none of them (when the stereo pair is already rectified)\n");
+        return -1;
+    }
+    
+    if( extrinsic_filename == 0 && point_cloud_filename )
+    {
+        printf("Command-line parameter error: extrinsic and intrinsic parameters must be specified to compute the point cloud\n");
+        return -1;
+    }
+    
+    int color_mode = alg == STEREO_BM ? 0 : -1;
+    Mat img1 = imread(img1_filename, color_mode);
+    Mat img2 = imread(img2_filename, color_mode);
+    Size img_size = img1.size();
+    
+    Rect roi1, roi2;
+    Mat Q;
+    
+    if( intrinsic_filename )
+    {
+        // reading intrinsic parameters
+        FileStorage fs(intrinsic_filename, CV_STORAGE_READ);
+        if(!fs.isOpened())
+        {
+            printf("Failed to open file %s\n", intrinsic_filename);
+            return -1;
+        }
         
-    CvMemStorage* storage = cvCreateMemStorage();
-    // reading intrinsic parameters
-    CvFileStorage* fstorage = cvOpenFileStorage(intrinsic_filename, storage, CV_STORAGE_READ);
-    if(!fstorage)
-    {
-        printf("Failed to open file %s\n", intrinsic_filename);
-        return 0;
-    }
-    CvMat* M1 = (CvMat*)cvReadByName(fstorage, NULL, "M1");
-    CvMat* D1 = (CvMat*)cvReadByName(fstorage, NULL, "D1");
-    CvMat* M2 = (CvMat*)cvReadByName(fstorage, NULL, "M2");
-    CvMat* D2 = (CvMat*)cvReadByName(fstorage, NULL, "D2");
-    cvReleaseFileStorage(&fstorage);
-    if(!M1 || !D1 || !M2 || !D2)
-    {
-        printf("Failed to read intrinsic parameters from %s\n", intrinsic_filename);
-        return 0;
-    }
-    
-    // reading extrinsic parameters
-    fstorage = cvOpenFileStorage(extrinsic_filename, storage, CV_STORAGE_READ);
-    if(!fstorage)
-    {
-        printf("Failed to open file %s\n", extrinsic_filename);
-        return 0;
-    }
-    CvMat* R1 = (CvMat*)cvReadByName(fstorage, NULL, "R1");
-    CvMat* P1 = (CvMat*)cvReadByName(fstorage, NULL, "P1");
-    CvMat* R2 = (CvMat*)cvReadByName(fstorage, NULL, "R2");
-    CvMat* P2 = (CvMat*)cvReadByName(fstorage, NULL, "P2");
-    CvMat* Q = (CvMat*)cvReadByName(fstorage, NULL, "Q");
-    cvReleaseFileStorage(&fstorage);
-    if(!R1 || !P1 || !R2 || !P2 || !Q)
-    {
-        printf("Failed to read extrinsic parameters from %s\n", extrinsic_filename);
-        return 0;
-    }
-    
-    CvMat* mx1 = NULL;
-    CvMat* mx2 = NULL;
-    CvMat* my1 = NULL;
-    CvMat* my2 = NULL;
-    CvMat* img1_rect = NULL;
-    CvMat* img2_rect = NULL;
-            
-    IplImage* img1 = cvLoadImage(img1_filename, CV_LOAD_IMAGE_GRAYSCALE);
-    IplImage* img2 = cvLoadImage(img2_filename, CV_LOAD_IMAGE_GRAYSCALE);
-    
-#if 0
-    IplImage* _img1 = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 1);
-    IplImage* _img2 = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 1);
-                                    
-    cvResize(img1, _img1);
-    cvResize(img2, _img2);
-    cvReleaseImage(&img1);
-    cvReleaseImage(&img2);
-    img1 = _img1;
-    img2 = _img2;
-#endif
-    
-    CvSize imageSize = cvGetSize(img1);
-    
-    if(mx1 == NULL)
-    {
-        mx1 = cvCreateMat(imageSize.height, imageSize.width, CV_32F);
-        mx2 = cvCreateMat(imageSize.height, imageSize.width, CV_32F);
-        my1 = cvCreateMat(imageSize.height, imageSize.width, CV_32F);
-        my2 = cvCreateMat(imageSize.height, imageSize.width, CV_32F);
-        cvInitUndistortRectifyMap(M1, D1, R1, P1, mx1, my1);
-        cvInitUndistortRectifyMap(M2, D2, R2, P2, mx2, my2);
+        Mat M1, D1, M2, D2;
+        fs["M1"] >> M1;
+        fs["D1"] >> D1;
+        fs["M2"] >> M2;
+        fs["D2"] >> D2;
         
-        img1_rect = cvCreateMat(imageSize.height, imageSize.width, CV_8U);
-        img2_rect = cvCreateMat(imageSize.height, imageSize.width, CV_8U);
+        fs.open(extrinsic_filename, CV_STORAGE_READ);
+        if(!fs.isOpened())
+        {
+            printf("Failed to open file %s\n", extrinsic_filename);
+            return -1;
+        }
+        
+        Mat R, T, R1, P1, R2, P2;
+        fs["R"] >> R;
+        fs["T"] >> T;
+        
+        stereoRectify( M1, D1, M2, D2, img_size, R, T, R1, R2, P1, P2, Q, -1, img_size, &roi1, &roi2 );
+        
+        Mat map11, map12, map21, map22;
+        initUndistortRectifyMap(M1, D1, R1, P1, img_size, CV_16SC2, map11, map12);
+        initUndistortRectifyMap(M2, D2, R2, P2, img_size, CV_16SC2, map21, map22);
+        
+        Mat img1r, img2r;
+        remap(img1, img1r, map11, map12, INTER_LINEAR);
+        remap(img2, img2r, map21, map22, INTER_LINEAR);
+        
+        img1 = img1r;
+        img2 = img2r;
     }
     
-    cvRemap(img1, img1_rect, mx1, my1);
-    cvRemap(img2, img2_rect, mx2, my2);
+    bm.state->roi1 = roi1;
+    bm.state->roi2 = roi2;
+    bm.state->preFilterCap = 31;
+    bm.state->SADWindowSize = SADWindowSize > 0 ? SADWindowSize : 11;
+    bm.state->minDisparity = 0;
+    bm.state->numberOfDisparities = numberOfDisparities > 0 ? numberOfDisparities : img_size.width/8;
+    bm.state->textureThreshold = 10;
+    bm.state->uniquenessRatio = 15;
+    bm.state->speckleWindowSize = 100;
+    bm.state->speckleRange = 32;
     
-    CvMat* disp = cvCreateMat( imageSize.height,
-                              imageSize.width, CV_16S );
-    CvMat* vdisp = cvCreateMat( imageSize.height,
-                               imageSize.width, CV_8U );
-    CvMat* dispn = cvCreateMat( imageSize.height, imageSize.width, CV_32F );
+    sgbm.preFilterCap = 63;
+    sgbm.SADWindowSize = SADWindowSize > 0 ? SADWindowSize : 7;
+    sgbm.P1 = 2*sgbm.SADWindowSize*sgbm.SADWindowSize;
+    sgbm.P2 = 5*sgbm.SADWindowSize*sgbm.SADWindowSize;
+    sgbm.minDisparity = 0;
+    sgbm.numberOfDisparities = bm.state->numberOfDisparities;
+    sgbm.uniquenessRatio = 10;
+    sgbm.speckleWindowSize = bm.state->speckleWindowSize;
+    sgbm.speckleRange = bm.state->speckleRange;
+    sgbm.fullDP = alg == STEREO_HH;
     
-    CvStereoBMState *BMState = cvCreateStereoBMState();
-    assert(BMState != 0);
-    BMState->preFilterSize=31;
-    BMState->preFilterCap=31;
-    BMState->SADWindowSize=15;//255;
-    BMState->minDisparity=-192;
-    BMState->numberOfDisparities=192;
-    BMState->textureThreshold=10;
-    BMState->uniquenessRatio=15;
-    
-#if 1
-    BMState->minDisparity *= imageSize.width/640;
-    BMState->numberOfDisparities *= imageSize.width/640;
-#endif
-    
-    int64 _time1 = cvGetTickCount();
-    cvFindStereoCorrespondenceBM( img1_rect, img2_rect, disp,
-                                 BMState);
-    int64 _time2 = cvGetTickCount();
-    printf("Time elapsed: %f\n", float(_time2 - _time1)/cvGetTickFrequency()*1e-6);
-        
-    // hack to get rid of small-scale noise
-    cvErode(disp, disp, NULL, 2);
-    cvDilate(disp, disp, NULL, 2);
-    
-    // should be removed when BM algorithm starts returning disparities rather than disparities*16
-    cvConvertScale(disp, dispn, 1.0/16);
-    
+    Mat disp, disp8;
+    int64 t = getTickCount();
+    if( alg == STEREO_BM )
+        bm(img1, img2, disp);
+    else
+        sgbm(img1, img2, disp);
+    t = getTickCount() - t;
+    printf("Time elapsed: %fms\n", t*1000/getTickFrequency());
 
-    if(point_cloud_filename)
+    disp.convertTo(disp8, CV_8U, 255/(numberOfDisparities*16.));
+    if( !no_display )
     {
-        CvMat* xyz = cvCreateMat(disp->rows, disp->cols, CV_32FC3);
-        cvReprojectImageTo3D(dispn, xyz, Q, 1);
-        
-        saveXYZ(point_cloud_filename, xyz);
-        cvReleaseMat(&xyz);
+        namedWindow("disparity", 1);
+        imshow("disparity", disp8);
+        printf("press any key to continue...");
+        fflush(stdout);
+        waitKey();
+        printf("\n");
     }
-    
-    cvNormalize( disp, vdisp, 0, 256, CV_MINMAX );
-    cvNamedWindow( "disparity" );
-    IplImage* vdisp1 = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 1);
-    cvResize(vdisp, vdisp1);
-    cvShowImage( "disparity", vdisp1 );
-    cvWaitKey();
     
     if(disparity_filename)
+        imwrite(disparity_filename, disp8);
+    
+    if(point_cloud_filename)
     {
-        cvSaveImage(disparity_filename, vdisp);
+        printf("storing the point cloud...");
+        fflush(stdout);
+        Mat xyz;
+        reprojectImageTo3D(disp, xyz, Q, true);
+        saveXYZ(point_cloud_filename, xyz);
+        printf("\n");
     }
     
-    cvReleaseImage(&vdisp1);
-    cvReleaseMat(&disp);
-    cvReleaseMat(&dispn);
-    cvReleaseMat(&vdisp);
-    cvReleaseImage(&img1);
-    cvReleaseImage(&img2);
-    
-    cvReleaseMat(&M1);
-    cvReleaseMat(&D1);
-    cvReleaseMat(&M2);
-    cvReleaseMat(&D2);
-    
-    cvReleaseMat(&R1);
-    cvReleaseMat(&P1);
-    cvReleaseMat(&R2);
-    cvReleaseMat(&P2);
-    cvReleaseMat(&Q);
-    
-    cvReleaseMat(&mx1);
-    cvReleaseMat(&my1);
-    cvReleaseMat(&mx2);
-    cvReleaseMat(&my2);
-    cvReleaseMat(&img1_rect);
-    cvReleaseMat(&img2_rect);
-    
-    cvReleaseMemStorage(&storage);
-    
+    return 0;
 }
