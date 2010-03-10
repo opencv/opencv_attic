e142c498a531969a8f073b7f31b77978ba468e9f
James Bowman <no@email>
2010-03-10 00:41:27 +0000

    generated CvHaarClassifierCascade
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2769 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/interfaces/python/gen.py
+++ opencv/interfaces/python/gen.py
@@ -424,6 +424,7 @@ objects = [
         "anchorY" : 'i',
     }),
     ( 'CvCapture', {}),
+    ( 'CvHaarClassifierCascade', {}),
     ( 'CvPOSITObject', {}),
     ( 'CvVideoWriter', {}),
     ( 'CvStereoBMState', {
