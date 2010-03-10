e938729dd352d3aecd1c733951c3fec763fea0b6
James Bowman <no@email>
2010-03-10 23:08:35 +0000

    kmeans
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@2783 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- samples/python/kmeans.py
+++ samples/python/kmeans.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         cv.CV_RGB(100, 100, 255),
         cv.CV_RGB(255, 0,255),
         cv.CV_RGB(255, 255, 0)]
-    img = cv.CreateImage(cv.Size(500, 500), 8, 3)
+    img = cv.CreateImage((500, 500), 8, 3)
     rng = cv.RNG(-1)
 
     cv.NamedWindow("clusters", 1)
@@ -25,9 +25,7 @@ if __name__ == "__main__":
         
         # generate random sample from multigaussian distribution
         for k in range(cluster_count):
-            center = CvPoint()
-            center.x = cv.RandInt(rng)%img.width
-            center.y = cv.RandInt(rng)%img.height
+            center = (cv.RandInt(rng)%img.width, cv.RandInt(rng)%img.height)
             first = k*sample_count/cluster_count
             last = sample_count
             if k != cluster_count:
@@ -36,31 +34,27 @@ if __name__ == "__main__":
             point_chunk = cv.GetRows(points, first, last)
                         
             cv.RandArr(rng, point_chunk, cv.CV_RAND_NORMAL,
-                       cv.Scalar(center.x, center.y, 0,0),
-                       cv.Scalar(img.width*0.1, img.height*0.1, 0,0))
+                       cv.Scalar(center[0], center[1], 0, 0),
+                       cv.Scalar(img.width*0.1, img.height*0.1, 0, 0))
         
 
         # shuffle samples 
         cv.RandShuffle(points, rng)
 
         cv.KMeans2(points, cluster_count, clusters,
-                   cv.TermCriteria(cv.CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0))
+                   (cv.CV_TERMCRIT_EPS + cv.CV_TERMCRIT_ITER, 10, 1.0))
 
         cv.Zero(img)
 
         for i in range(sample_count):
-            cluster_idx = clusters[i]
-            # a multi channel matrix access returns a scalar of
-            #dimension 4, 0, which is not considerate a 
-            #we have to create a tuple with the first two elements
-            pt = (cv.Round(points[i][0]), cv.Round(points[i][1]))
+            cluster_idx = int(clusters[i, 0])
+            pt = (cv.Round(points[i, 0][0]), cv.Round(points[i, 0][1]))
             cv.Circle(img, pt, 2, color_tab[cluster_idx], cv.CV_FILLED, cv. CV_AA, 0)
 
         cv.ShowImage("clusters", img)
 
         key = cv.WaitKey(0)
-        if(key == 27 or key == 'q' or key == 'Q'): # 'ESC'
+        if key in [27, ord('q'), ord('Q')]:
             break
-    
     
     cv.DestroyWindow("clusters")
