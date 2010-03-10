f8a067c15129bff45d19a2231abcf9591f4a9296
James Bowman <no@email>
2010-03-10 23:38:46 +0000

    minidemo
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2788 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/samples/python/minidemo.py
+++ opencv/samples/python/minidemo.py
@@ -3,12 +3,11 @@
 import urllib2
 import cv
 
-cap = highgui.cv.CreateFileCapture("../c/tree.avi")
-img = highgui.cv.QueryFrame(cap)
+cap = cv.CreateFileCapture("../samples/c/tree.avi")
+img = cv.QueryFrame(cap)
 print "Got frame of dimensions (", img.width, " x ", img.height, ")"
 
-highgui.cv.NamedWindow("win", highgui.cv.CV_WINDOW_AUTOSIZE)
-highgui.cv.ShowImage("win", img)
-highgui.cv.MoveWindow("win", 200, 200)
-highgui.cv.WaitKey(0)
-
+cv.NamedWindow("win", highgui.cv.CV_WINDOW_AUTOSIZE)
+cv.ShowImage("win", img)
+cv.MoveWindow("win", 200, 200)
+cv.WaitKey(0)
