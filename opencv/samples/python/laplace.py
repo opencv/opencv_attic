c8652fce0e45108324317d92b24b987d27feeeaf
James Bowman <no@email>
2010-03-10 23:12:19 +0000

    laplace
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2784 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/samples/python/laplace.py
+++ opencv/samples/python/laplace.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     planes = [ None, None, None ]
     capture = None
     
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         capture = cv.CreateCameraCapture(0)
-    elif len(sys.argv)==2 and sys.argv[1].isdigit():
+    elif len(sys.argv) == 2 and sys.argv[1].isdigit():
         capture = cv.CreateCameraCapture(int(sys.argv[1]))
-    elif len(sys.argv)==2:
+    elif len(sys.argv) == 2:
         capture = cv.CreateFileCapture(sys.argv[1]) 
 
     if not capture:
@@ -24,24 +24,20 @@ if __name__ == "__main__":
 
     while True:
         frame = cv.QueryFrame(capture)
-        if not frame:
-            cv.WaitKey(0)
-            break
+        if frame:
+            if not laplace:
+                planes = [cv.CreateImage((frame.width, frame.height), 8, 1) for i in range(3)]
+                laplace = cv.CreateImage((frame.width, frame.height), cv.IPL_DEPTH_16S, 1)
+                colorlaplace = cv.CreateImage((frame.width, frame.height), 8, 3)
 
-        if not laplace:
-            for i in range(len(planes)):
-                planes[i] = cv.CreateImage(cv.Size(frame.width, frame.height), 8, 1)
-            laplace = cv.CreateImage(cv.Size(frame.width, frame.height), IPL_DEPTH_16S, 1)
-            colorlaplace = cv.CreateImage(cv.Size(frame.width, frame.height), 8, 3)
+            cv.Split(frame, planes[0], planes[1], planes[2], None)
+            for plane in planes:
+                cv.Laplace(plane, laplace, 3)
+                cv.ConvertScaleAbs(laplace, plane, 1, 0)
 
-        cv.Split(frame, planes[0], planes[1], planes[2], None)
-        for plane in planes:
-            cv.Laplace(plane, laplace, 3)
-            cv.ConvertScaleAbs(laplace, plane, 1, 0)
+            cv.Merge(planes[0], planes[1], planes[2], None, colorlaplace)
 
-        cv.Merge(planes[0], planes[1], planes[2], None, colorlaplace)
-
-        cv.ShowImage("Laplacian", colorlaplace)
+            cv.ShowImage("Laplacian", colorlaplace)
 
         if cv.WaitKey(10) != -1:
             break
