7f7fe695b28d18da55fbc10106e5166f62e6a63d
James Bowman <no@email>
2010-03-10 23:17:16 +0000

    logpolar
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@2786 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- samples/python/logpolar.py
+++ samples/python/logpolar.py
@@ -9,25 +9,25 @@ src2=None
 
 def on_mouse(event, x, y, flags, param):
 
-    if(not src):
+    if not src:
         return
 
     if event==cv.CV_EVENT_LBUTTONDOWN:
-        cv.LogPolar(src, dst, 2D32f(x, y), 40, cv.CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS)
-        cv.LogPolar(dst, src2, 2D32f(x, y), 40, cv.CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS+CV_WARP_INVERSE_MAP)
+        cv.LogPolar(src, dst, (x, y), 40, cv.CV_INTER_LINEAR + cv.CV_WARP_FILL_OUTLIERS)
+        cv.LogPolar(dst, src2, (x, y), 40, cv.CV_INTER_LINEAR + cv.CV_WARP_FILL_OUTLIERS + cv.CV_WARP_INVERSE_MAP)
         cv.ShowImage("log-polar", dst)
         cv.ShowImage("inverse log-polar", src2)
 
 if __name__ == "__main__":
     
-    filename = "../c/fruits.jpg"
-    if len(sys.argv)>1:
-        filename=argv[1]
-    
-    src = cv.LoadImage(filename, 1)
-    if not src:
-        print "Could not open %s" % filename
-        sys.exit(-1)
+    if len(sys.argv) > 1:
+        src = cv.LoadImage( sys.argv[1], cv.CV_LOAD_IMAGE_COLOR)
+    else:
+        url = 'https://code.ros.org/svn/opencv/trunk/opencv/samples/c/fruits.jpg'
+        filedata = urllib2.urlopen(url).read()
+        imagefiledata = cv.CreateMatHeader(1, len(filedata), cv.CV_8UC1)
+        cv.SetData(imagefiledata, filedata, len(filedata))
+        src = cv.DecodeImage(imagefiledata, cv.CV_LOAD_IMAGE_COLOR)
         
     cv.NamedWindow("original", 1)
     cv.NamedWindow("log-polar", 1)
