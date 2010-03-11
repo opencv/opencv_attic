7cb50677c0abfc75f1e99ad19970a7a8147cd6ad
James Bowman <no@email>
2010-03-11 00:16:46 +0000

    motempl
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2789 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/samples/python/motempl.py
+++ opencv/samples/python/motempl.py
@@ -25,18 +25,18 @@ def update_mhi(img, dst, diff_threshold):
     global mask
     global orient
     global segmask
-    timestamp = time.clock()/CLOCKS_PER_SEC # get current time in seconds
-    size = (img.width, img.height) # get current frame size
+    timestamp = time.clock() / CLOCKS_PER_SEC # get current time in seconds
+    size = cv.GetSize(img) # get current frame size
     idx1 = last
-    if not mhi or mhi.width != size.width or mhi.height != size.height: 
+    if not mhi or cv.GetSize(mhi) != size:
         for i in range(N):
-            buf[i] = cv.CreateImage(size, IPL_DEPTH_8U, 1)
+            buf[i] = cv.CreateImage(size, cv.IPL_DEPTH_8U, 1)
             cv.Zero(buf[i])
-        mhi = cv.CreateImage(size, IPL_DEPTH_32F, 1)
+        mhi = cv.CreateImage(size,cv. IPL_DEPTH_32F, 1)
         cv.Zero(mhi) # clear MHI at the beginning
-        orient = cv.CreateImage(size, IPL_DEPTH_32F, 1)
-        segmask = cv.CreateImage(size, IPL_DEPTH_32F, 1)
-        mask = cv.CreateImage(size, IPL_DEPTH_8U, 1)
+        orient = cv.CreateImage(size,cv. IPL_DEPTH_32F, 1)
+        segmask = cv.CreateImage(size,cv. IPL_DEPTH_32F, 1)
+        mask = cv.CreateImage(size,cv. IPL_DEPTH_8U, 1)
     
     cv.CvtColor(img, buf[last], cv.CV_BGR2GRAY) # convert frame to grayscale
     idx2 = (last + 1) % N # index of (last - (N-1))th frame
@@ -50,36 +50,33 @@ def update_mhi(img, dst, diff_threshold):
     cv.Zero(dst)
     cv.Merge(mask, None, None, None, dst)
     cv.CalcMotionGradient(mhi, mask, orient, MAX_TIME_DELTA, MIN_TIME_DELTA, 3)
-    if(not storage):
+    if not storage:
         storage = cv.CreateMemStorage(0)
-    else:
-        cv.ClearMemStorage(storage)
     seq = cv.SegmentMotion(mhi, segmask, storage, timestamp, MAX_TIME_DELTA)
-    for i in range(-1, seq.total):
-        if(i < 0):  # case of the whole image
-            comp_rect = cv.Rect(0, 0, size.width, size.height)
-            color = cv.CV_RGB(255, 255, 255)
-            magnitude = 100.
-        else:  # i-th motion component
-            comp_rect = seq[i].rect 
-            if(comp_rect.width + comp_rect.height < 100): # reject very small components
-                continue
+    for (area, value, comp_rect) in seq:
+        if comp_rect[2] + comp_rect[3] > 100: # reject very small components
             color = cv.CV_RGB(255, 0,0)
+            silh_roi = cv.GetSubRect(silh, comp_rect)
+            mhi_roi = cv.GetSubRect(mhi, comp_rect)
+            orient_roi = cv.GetSubRect(orient, comp_rect)
+            mask_roi = cv.GetSubRect(mask, comp_rect)
+            angle = 360 - cv.CalcGlobalOrientation(orient_roi, mask_roi, mhi_roi, timestamp, MHI_DURATION)
+
+            count = cv.Norm(silh_roi, None, cv.CV_L1, None) # calculate number of points within silhouette ROI
+            if count < (comp_rect[2] * comp_rect[3] * 0.05):
+                continue
+
             magnitude = 30.
-        silh_roi = cv.GetSubRect(silh, comp_rect)
-        mhi_roi = cv.GetSubRect(mhi, comp_rect)
-        orient_roi = cv.GetSubRect(orient, comp_rect)
-        mask_roi = cv.GetSubRect(mask, comp_rect)
-        angle = cv.CalcGlobalOrientation(orient_roi, mask_roi, mhi_roi, timestamp, MHI_DURATION)
-        angle = 360.0 - angle  # adjust for images with top-left origin
-        count = cv.Norm(silh_roi, None, cv.CV_L1, None) # calculate number of points within silhouette ROI
-        if(count < comp_rect.width * comp_rect.height * 0.05):
-            continue
-        center = ((comp_rect.x + comp_rect.width/2),
-                          (comp_rect.y + comp_rect.height/2))
-        cv.Circle(dst, center, cv.Round(magnitude*1.2), color, 3, cv.CV_AA, 0)
-        cv.Line(dst, center, (cv.Round(center.x + magnitude*cos(angle*cv.CV_PI/180)),
-                cv.Round(center.y - magnitude*sin(angle*cv.CV_PI/180))), color, 3, cv. CV_AA, 0)
+            center = ((comp_rect[0] + comp_rect[2] / 2), (comp_rect[1] + comp_rect[3] / 2))
+            cv.Circle(dst, center, cv.Round(magnitude*1.2), color, 3, cv.CV_AA, 0)
+            cv.Line(dst,
+                    center,
+                    (cv.Round(center[0] + magnitude * cos(angle * cv.CV_PI / 180)),
+                     cv.Round(center[1] - magnitude * sin(angle * cv.CV_PI / 180))),
+                    color,
+                    3,
+                    cv.CV_AA,
+                    0)
 
 if __name__ == "__main__":
     motion = 0
