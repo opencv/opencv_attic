13f84d98750340189f7e9691ae1ae80f28434883
James Bowman <no@email>
2010-03-15 18:54:57 +0000

    Checkpoint
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@2822 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/samples/python/pyramid_segmentation.py
+++ opencv/samples/python/pyramid_segmentation.py
@@ -9,7 +9,6 @@ image1 = None
 threshold1 = 255
 threshold2 = 30
 l = level = 4
-block_size = 1000
 filter = cv.CV_GAUSSIAN_5x5
 storage = None
 min_comp = CvConnectedComp()
@@ -31,20 +30,19 @@ def ON_SEGMENT():
     cv.ShowImage("Segmentation", image1)
 
 if __name__ == "__main__":
-    filename = "../c/fruits.jpg"
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
-    image[0] = cv.LoadImage(filename, 1)
-    if not image[0]:
-        print "Error opening %s" % filename
-        sys.exit(-1)
+    if len(sys.argv) > 1:
+        img0 = cv.LoadImage( sys.argv[1], cv.cv.CV_LOAD_IMAGE_COLOR)
+    else:
+        url = 'https://code.ros.org/svn/opencv/trunk/opencv/samples/c/fruits.jpg'
+        filedata = urllib2.urlopen(url).read()
+        imagefiledata = cv.CreateMatHeader(1, len(filedata), cv.CV_8UC1)
+        cv.SetData(imagefiledata, filedata, len(filedata))
+        img0 = cv.DecodeImage(imagefiledata, cv.CV_LOAD_IMAGE_COLOR)
 
     cv.NamedWindow("Source", 0)
     cv.ShowImage("Source", image[0])
     cv.NamedWindow("Segmentation", 0)
-    storage = cv.CreateMemStorage (block_size)
-    image[0].width &= -(1<<level)
-    image[0].height &= -(1<<level)
+    storage = cv.CreateMemStorage()
     image0 = cv.CloneImage(image[0])
     image1 = cv.CloneImage(image[0])
     # segmentation of the color image
