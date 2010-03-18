1a860cdf4001636b51cc9d72175b66229b629fe2
James Bowman <no@email>
2010-03-18 22:54:58 +0000

    Many fixes for Python found by new tool; added Set?D and Get?D for Python
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@2857 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- doc/latex2sphinx/conf.py
+++ doc/latex2sphinx/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'opencv'
-copyright = u'2009, authors'
+copyright = u'2010, authors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- doc/latex2sphinx/latex.py
+++ doc/latex2sphinx/latex.py
@@ -1,101 +1,22 @@
 import sys
-from pyparsing import Word, CharsNotIn, Optional, OneOrMore, ZeroOrMore, Group, Forward, ParseException, Literal, Suppress, replaceWith, StringEnd, lineno, QuotedString
+from latexparser import latexparser, TexCmd
+import distutils.dep_util
+import os
+import cPickle as pickle
+import pyparsing as pp
+import StringIO
+from qfile import QOpen
 
-class Argument:
-    def __init__(self, s, loc, toks):
-        self.str = toks[1]
-    def __repr__(self):
-        return "[%s]" % self.str
-    def __str__(self):
-        return self.str
-def argfun(s, loc, toks):
-    return Argument(s, loc, toks)
-
-class Parameter:
-    def __init__(self, s, loc, toks):
-        self.str = toks[0].asList()
-    def __repr__(self):
-        return '{' + "".join([str(s) for s in self.str]) + '}'
-        return "{%s}" % self.str
-    def __str__(self):
-        return "".join([str(s) for s in self.str])
-def paramfun(s, loc, toks):
-    return Parameter(s, loc, toks)
-
-class TexCmd:
-    def __init__(self, s, loc, toks):
-        self.cmd = str(toks[0])[1:]
-        #print 'cmd', self.cmd
-        self.args = toks[1].asList()
-        self.params = toks[2].asList()
-        self.lineno = lineno(loc, s)
-        self.filename = None
-    def __repr__(self):
-        return '\\' + self.cmd + "".join([repr(a) for a in self.args]) + "".join([repr(p) for p in self.params])
-
-class ZeroOrMoreAsList(ZeroOrMore):
-    def __init__(self, *args):
-        ZeroOrMore.__init__(self, *args)
-        def listify(s, loc, toks):
-            return [toks]
-        self.setParseAction(listify)
-
-backslash = chr(92)
-
-texcmd = Forward()
-filler = CharsNotIn(backslash + '\n')
-filler2 = CharsNotIn(backslash + '\n' + '}')
-
-arg = '[' + CharsNotIn("]") + ']'
-arg.setParseAction(argfun)
-param = Suppress(Literal('{')) + ZeroOrMoreAsList(filler2 | texcmd) + Suppress(Literal('}'))
-param.setParseAction(paramfun)
-texcmd << Word("\\", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]") + ZeroOrMoreAsList(arg) + ZeroOrMoreAsList(param)
-def texcmdfun(s, loc, toks):
-    if str(toks[0])[1:] == 'input':
-        filename = "../" + toks[2].asList()[0].str[0] + ".tex"
-        print 'Now parsing', filename, loc
-        return parsefile(filename, lineno(loc, s))
-    else:
-        return TexCmd(s, loc, toks)
-texcmd.setParseAction(texcmdfun)
-
-#legal = "".join([chr(x) for x in set(range(32, 127)) - set(backslash)])
-#filler = Word(legal)
-document = ZeroOrMore(texcmd | filler) + StringEnd().suppress()
-
-if 0:
-    print document.parseString("(represented as \\cvCppCross{Mat}'s)")
-    print document.parseString("(represented as \\cvCppCross{Mat \\foo{bar}}'s)")
-    sys.exit(0)
-
-def parsefile(filename, startline):
-    f = open(filename, "rt")
-
-    lines = list(f)
-    def uncomment(s):
-        if '%' in s:
-            return s[:s.index('%')] + '\n'
-        else:
-            return s
-
-    lines = [uncomment(l) for l in lines]
-    print len(lines), "lines"
-
-    docstr = "".join(lines).replace('\\_', '_')
-    # document.setFailAction(None)
-    try:
-        r = document.parseString(docstr)
-        for x in r:
-            if isinstance(x, TexCmd) and not x.filename:
-                x.filename = filename
-        return r
-    except ParseException, pe:
-        print 'Fatal problem at %s line %d col %d' % (filename, pe.lineno, pe.col)
-        print pe.line
-        sys.exit(1)
-
-fulldoc = parsefile(sys.argv[1], 0)
+sources = ['../' + f for f in os.listdir('..') if f.endswith('.tex')]
+if distutils.dep_util.newer_group(["latexparser.py"] + sources, "pickled"):
+    fulldoc = latexparser(sys.argv[1], 0)
+    pickle.dump(fulldoc, open("pickled", 'wb'))
+    raw = open('fulldoc', 'w')
+    for x in fulldoc:
+        print >>raw, repr(x)
+    raw.close()
+else:
+    fulldoc = pickle.load(open("pickled", "rb"))
 
 # Filter on target language
 def preprocess_conditionals(fd, conditionals):
@@ -107,7 +28,7 @@ def preprocess_conditionals(fd, conditionals):
             loc = (x.filename, x.lineno)
             if ll.startswith("if"):
                 # print " " * len(ifstack), '{', loc
-                ifstack.append((conditionals.get(ll[3:], False), loc))
+                ifstack.append((conditionals.get(ll[2:], False), loc))
             elif ll.startswith("else"):
                 ifstack[-1] = (not ifstack[-1][0], ifstack[-1][1])
             elif ll.startswith("fi"):
@@ -139,23 +60,36 @@ raw.close()
 
 class SphinxWriter:
     def __init__(self, filename):
-        self.f_index = open(filename, 'wt')
+        self.f_index = QOpen(filename, 'wt')
         self.f = self.f_index
         self.f_chapter = None
         self.f_section = None
         self.indent = 0
         self.state = None
         self.envstack = []
-        self.tags = open('TAGS', 'w')
+        self.tags = []
+        self.errors = open('errors', 'wt')
+        self.unhandled_commands = set()
+        self.freshline = True
 
     def write(self, s):
+        self.freshline = len(s) > 0 and (s[-1] == '\n')
         self.f.write(s.replace('\n', '\n' + self.indent * "    "))
 
+    def appendspace(self):
+        """ append a space to the output - if we're not at the start of a line """
+        if not self.freshline:
+            self.write(' ')
+
     def doplain(self, s):
+        if (len(s) > 1) and (s[0] == '$' and s[-1] == '$'):
+            s = ":math:`%s`" % s[1:-1]
+        elif self.state != 'math':
+            s.replace('\\_', '_')
         if self.state == 'fpreamble':
             self.description += s
         else:
-            print >>self, s
+            self.write(s)
 
     def docmd(self, c):
         if self.state == 'math':
@@ -164,25 +98,25 @@ class SphinxWriter:
             else:
                 self.indent -= 1
                 self.state = None
-                print >>self
+                self.write('\n\n')
         else:
             if c.cmd == '[':
                 meth = self.cmd_gomath
             else:
                 cname = "cmd_" + c.cmd
-                meth = getattr(self, cname, self.default_cmd)
+                meth = getattr(self, cname, self.unrecognized_cmd)
             meth(c)
 
     def cmd_gomath(self, c):
         self.state = 'math'
-        print >>self, "\n.. math::"
+        print >>self, "\n\n.. math::"
         self.indent += 1
         print >>self
 
     def cmd_chapter(self, c):
         filename = str(c.params[0]).lower().replace(' ', '_').replace('/','_')
         self.f_index.write("    %s\n" % filename)
-        self.f_chapter = open(filename + '.rst', 'wt')
+        self.f_chapter = QOpen(filename + '.rst', 'wt')
         self.f_section = None
         self.f = self.f_chapter
         self.indent = 0
@@ -198,7 +132,7 @@ class SphinxWriter:
     def cmd_section(self, c):
         filename = str(c.params[0]).lower().replace(' ', '_').replace('/','_')
         self.f_chapter.write("    %s\n" % filename)
-        self.f_section = open(filename + '.rst', 'wt')
+        self.f_section = QOpen(filename + '.rst', 'wt')
         self.f = self.f_section
         self.indent = 0
         title = str(c.params[0])
@@ -206,14 +140,27 @@ class SphinxWriter:
         print >>self, '=' * len(title)
         print >>self
 
+    def cmd_subsection(self, c):
+        print >>self, str(c.params[0])
+        print >>self, '-' * len(str(c.params[0]))
+        print >>self
+        self.function_props = {}
+
+    def cmd_includegraphics(self, c):
+        filename = '../' + str(c.params[0])
+        if not os.path.isfile(filename):
+            print >>self.errors, "WARNING: missing image file", filename
+        print >>self, "\n\n.. image:: %s\n\n" % filename
+
     def cmd_cvCppCross(self, c):
-        print >>self, ":ref:`%s`" % str(c.params[0])
+        self.write(":ref:`%s`" % str(c.params[0]))
+
     def cmd_cvCPyCross(self, c):
-        print >>self, ":ref:`%s`" % str(c.params[0])
+        self.write(":ref:`%s`" % str(c.params[0]))
 
     def cmd_cvCPyFunc(self, c):
         self.indent = 0
-        nm = str(c.params[0])
+        nm = self.render(c.params[0].str)
         print >>self, "\n.. index:: %s\n" % nm
         print >>self, ".. _%s:\n" % nm
         print >>self, nm
@@ -221,73 +168,184 @@ class SphinxWriter:
         print >>self
         self.state = 'fpreamble'
         self.description = ""
-        print >>self.tags, "%s\t%s\t%d" % (nm, c.filename, c.lineno)
+        self.tags.append("%s\t%s\t%d" % (nm, c.filename, c.lineno))
+
+        self.function_props = {'name' : nm}
 
     def cmd_cvdefPy(self, c):
-        s = str(c.params[0])
+        s = str(c.params[0]).replace('\\_', '_')
         print >>self, ".. function:: " + s + "\n"
-        self.indent = 1
+        # print >>self, "=", repr(c.params[0].str)
+        self.indent = 0
         print >>self, self.description
         print >>self
         self.state = None
+        self.function_props['defpy'] = s
+
+    def report_error(self, c, msg):
+        print >>self.errors, "%s:%d: Error %s" % (c.filename, c.lineno, msg)
 
     def cmd_begin(self, c):
+        self.write('\n')
         s = str(c.params[0])
         self.envstack.append((s, (c.filename, c.lineno)))
         if s == 'description':
-            pass
+            if 'name' in self.function_props and not 'defpy' in self.function_props:
+                self.report_error(c, "No cvdefPy for function %s" % self.function_props['name'])
+            self.indent += 1
+        elif s == 'lstlisting':
+            print >>self, "\n::\n"
+            self.indent += 1
+        elif s in ['itemize', 'enumerate']:
+            self.indent += 1
         else:
             self.default_cmd(c)
 
+    def cmd_item(self, c):
+        self.indent -= 1
+        markup = {'itemize' : '*', 'enumerate' : '#.', 'description' : '*'}[self.ee()[-1]]
+        if len(c.args) > 0:
+            markup += " " + str(c.args[0])
+        self.write("\n\n" + markup)
+        self.indent += 1
+
     def cmd_end(self, c):
+        self.write('\n')
         s = str(c.params[0])
         if self.envstack == []:
             print "Cannot pop at", (c.filename, c.lineno)
         self.envstack.pop()
         if s == 'description':
-            pass
+            self.indent -= 1
+        elif s in ['itemize', 'enumerate']:
+            self.indent -= 1
+        elif s == 'lstlisting':
+            print >>self
+            self.indent -= 1
         else:
             self.default_cmd(c)
         
+    def cmd_label(self, c):
+        pass
+
     def cmd_cvdefC(self, c):
         pass
     def cmd_cvdefCpp(self, c):
         pass
+
+    # Conditionals
     def cmd_cvCpp(self, c):
         pass
+    def cmd_cvC(self, c):
+        pass
+    def cmd_cvCPy(self, c):
+        self.doL(c.params[0].str, False)
+    def cmd_cvPy(self, c):
+        self.doL(c.params[0].str, False)
 
-    def cmd_cvarg(self, c):
-        ee = [n for (n,_) in self.envstack[1:]]
-        if ee == ['description']:
-            print >>self, '\n:param %s: ' % str(c.params[0]),
-        elif ee == ['description', 'description']:
-            print >>self, '\n* **%s** ' % str(c.params[0]),
-        else:
-            print self.envstack
-            assert 0
-        self.indent += 1
-        self.doL(c.params[1].str)
-        self.indent -= 1
-        print >>self
-
-    def cmd_texttt(self, c):
-        print >>self, "``" + str(c.params[0]) + "``"
-
-    def default_cmd(self, c):
-        if self.f == self.f_section:
-            print >>self, repr(c)
-
-    def doL(self, L):
+    def render(self, L):
+        """ return L rendered as a string """
+        save = self.f
+        self.f = StringIO.StringIO()
         for x in L:
             if isinstance(x, TexCmd):
                 self.docmd(x)
             else:
                 self.doplain(x)
+        r = self.f.getvalue()
+        self.f = save
+        return r
 
-# dststack[0] -> index.rst
-#         [1] -> current chapter
-#         [2] -> current section
+    def cmd_cvarg(self, c):
+        if self.ee() == ['description']:
+            print >>self, '\n:param %s: ' % self.render(c.params[0].str),
+        elif self.ee() == ['description', 'description']:
+            print >>self, '\n* **%s** ' % self.render(c.params[0].str),
+        else:
+            print self.envstack
+            assert 0
+        self.indent += 1
+        self.doL(c.params[1].str, False)
+        self.indent -= 1
+        print >>self
 
+    def cmd_genc(self, c): pass 
+    def cmd_genpy(self, c): pass 
+    def cmd_author(self, c): pass 
+    def cmd_cite(self, c): pass
+    def cmd_date(self, c): pass
+    def cmd_def(self, c): pass
+    def cmd_documentclass(self, c): pass
+    def cmd_maketitle(self, c): pass
+    def cmd_newcommand(self, c): pass
+    def cmd_newline(self, c): pass
+    def cmd_setcounter(self, c): pass
+    def cmd_tableofcontents(self, c): pass
+    def cmd_targetlang(self, c): pass
+    def cmd_usepackage(self, c): pass
+    def cmd_title(self, c): pass
+
+    def cmd_href(self, c):
+        self.write("`%s <%s>`_" % (str(c.params[1]), str(c.params[0])))
+
+    def cmd_url(self, c):
+        self.write(str(c.params[0]))
+
+    def cmd_emph(self, c):
+        self.write("*" + self.render(c.params[0].str) + "*")
+
+    def cmd_textit(self, c):
+        self.write("*" + self.render(c.params[0].str) + "*")
+
+    def cmd_textbf(self, c):
+        self.write("**" + self.render(c.params[0].str) + "**")
+
+    def cmd_texttt(self, c):
+        self.write("``" + self.render(c.params[0].str) + "``")
+
+    def default_cmd(self, c):
+        if self.f == self.f_section:
+            self.write(repr(c))
+
+    def unrecognized_cmd(self, c):
+        if self.f == self.f_section:
+            self.write(repr(c))
+        self.unhandled_commands.add(c.cmd)
+
+    def doL(self, L, newlines = True):
+        for x in L:
+            if isinstance(x, TexCmd):
+                self.docmd(x)
+            else:
+                if 'lstlisting' in self.ee() or not newlines:
+                    self.doplain(x)
+                else:
+                    self.doplain(x.lstrip())
+            if self.state in ['math'] or not newlines:
+                self.appendspace()
+            else:
+                self.write('\n')
+
+    def ee(self):
+        """ Return tags of the envstack.  envstack[0] is 'document', so skip it """
+        return [n for (n,_) in self.envstack[1:]]
+
+    def close(self):
+
+        print >>self.errors, "unrecognized commands:"
+        print >>self.errors, "\n    ".join(sorted(self.unhandled_commands))
+        print >>self.errors
+
+        open('TAGS', 'w').write("\n".join(sorted(self.tags)))
+
+        print >>self.f_index, """
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
+"""
 
 sr = SphinxWriter('index.rst')
 print >>sr, """
@@ -303,6 +361,7 @@ Contents:
 
 """
 sr.doL(doc)
+sr.close()
 sys.exit(0)
 
 structuring = ['chapter', 'section']
