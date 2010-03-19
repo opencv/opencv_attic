74c3e2ead29a7e4bc8cb253b9a35b9fcfe0508b6
James Bowman <no@email>
2010-03-19 01:07:26 +0000

    Contributed patches, #147
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@2860 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- samples/python/pyramid_segmentation.py
+++ samples/python/pyramid_segmentation.py
@@ -1,58 +1,40 @@
 #!/usr/bin/python
-import sys
-import urllib2
 import cv
 
-image =  [None, None]
-image0 = None
-image1 = None
-threshold1 = 255
-threshold2 = 30
-l = level = 4
-filter = cv.CV_GAUSSIAN_5x5
-storage = None
-min_comp = CvConnectedComp()
+class PyrSegmentation:
+    def __init__(self, img0):
+        self.thresh1 = 255
+        self.thresh2 = 30
+        self.level =4
+        self.storage = cv.CreateMemStorage()
+        cv.NamedWindow("Source", 0)
+        cv.ShowImage("Source", img0)
+        cv.NamedWindow("Segmentation", 0)
+        cv.CreateTrackbar("Thresh1", "Segmentation", self.thresh1, 255, self.set_thresh1)
+        cv.CreateTrackbar("Thresh2", "Segmentation",  self.thresh2, 255, self.set_thresh2)
+        self.image0 = cv.CloneImage(img0)
+        self.image1 = cv.CloneImage(img0)
+        cv.ShowImage("Segmentation", self.image1)
 
-def set_thresh1(val):
-    global threshold1
-    threshold1 = val
-    ON_SEGMENT()
+    def set_thresh1(self, val):
+        self.thresh1 = val
+        self.on_segment()
 
-def set_thresh2(val):
-    global threshold2
-    threshold2 = val
-    ON_SEGMENT()
+    def set_thresh2(self, val):
+        self.thresh2 = val
+        self.on_segment()
 
-def ON_SEGMENT():
-    global storage
-    global min_comp
-    comp = cv.PyrSegmentation(image0, image1, storage, level, threshold1+1, threshold2+1)
-    cv.ShowImage("Segmentation", image1)
+    def on_segment(self):
+        comp = cv.PyrSegmentation(self.image0, self.image1, self.storage, \
+                            self.level, self.thresh1+1, self.thresh2+1)
+        cv.ShowImage("Segmentation", self.image1)
+    
+    def run(self):
+        self.on_segment()
+        cv.WaitKey(0)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        img0 = cv.LoadImage( sys.argv[1], cv.CV_LOAD_IMAGE_COLOR)
-    else:
-        url = 'https://code.ros.org/svn/opencv/trunk/opencv/samples/c/fruits.jpg'
-        filedata = urllib2.urlopen(url).read()
-        imagefiledata = cv.CreateMatHeader(1, len(filedata), cv.CV_8UC1)
-        cv.SetData(imagefiledata, filedata, len(filedata))
-        img0 = cv.DecodeImage(imagefiledata, cv.CV_LOAD_IMAGE_COLOR)
+    img0 = cv.LoadImage("../c/fruits.jpg", 1)
 
-    cv.NamedWindow("Source", 0)
-    cv.ShowImage("Source", image[0])
-    cv.NamedWindow("Segmentation", 0)
-    storage = cv.CreateMemStorage()
-    image0 = cv.CloneImage(image[0])
-    image1 = cv.CloneImage(image[0])
     # segmentation of the color image
-    l = 1
-    threshold1 =255
-    threshold2 =30
-    ON_SEGMENT()
-    sthreshold1 = cv.CreateTrackbar("Threshold1", "Segmentation", threshold1, 255, set_thresh1)
-    sthreshold2 = cv.CreateTrackbar("Threshold2", "Segmentation",  threshold2, 255, set_thresh2)
-    cv.ShowImage("Segmentation", image1)
-    cv.WaitKey(0)
-    cv.DestroyWindow("Segmentation")
-    cv.DestroyWindow("Source")
+    PyrSegmentation(img0).run()
