2c6e59232c098e9e4996e6d950a8ff4fe0f5fea0
James Bowman <no@email>
2010-03-23 18:27:25 +0000

    Dummy _static for Sphinx
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk/opencv@2889 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- doc/latex2sphinx/conf.py
+++ doc/latex2sphinx/conf.py
@@ -99,7 +99,7 @@ html_theme = 'blue'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_themes']
+html_theme_path = ['../_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -110,7 +110,7 @@ html_theme_path = ['_themes']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'opencv-logo2.png'
+html_logo = '../opencv-logo2.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -120,7 +120,7 @@ html_logo = 'opencv-logo2.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
