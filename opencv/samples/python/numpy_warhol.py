0c20e694e5a434cf9021c74112f842690f943e57
James Bowman <no@email>
2010-04-10 00:51:59 +0000

    Numpy demo cleaned up
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@3004 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/samples/python/numpy_warhol.py
+++ opencv/samples/python/numpy_warhol.py
@@ -3,21 +3,6 @@ import urllib2
 import sys
 import cv
 import numpy
-import time
-
-# SRGB-linear conversions using NumPy - see http://en.wikipedia.org/wiki/SRGB
-
-def srgb2lin(x):
-    a = 0.055
-    return numpy.where(x <= 0.04045,
-                       x * (1.0 / 12.92),
-                       numpy.power((x + a) * (1.0 / (1 + a)), 2.4))
-
-def lin2srgb(x):
-    a = 0.055
-    return numpy.where(x <= 0.0031308,
-                       x * 12.92,
-                       (1 + a) * numpy.power(x, 1 / 2.4) - a)
 
 if __name__ == "__main__":
     cv.NamedWindow("camera", 1)
@@ -30,8 +15,7 @@ if __name__ == "__main__":
     bottomleft = numpy.asarray(cv.GetSubRect(paste, (0, 480, 640, 480)))
     bottomright = numpy.asarray(cv.GetSubRect(paste, (640, 480, 640, 480)))
 
-    started = time.time()
-    for i in range(100):
+    while True:
         img = cv.GetMat(cv.QueryFrame(capture))
 
         n = (numpy.asarray(img)).astype(numpy.uint8)
@@ -48,17 +32,16 @@ if __name__ == "__main__":
         topright[:,:,1] = 255 - red
         topright[:,:,2] = grn
 
-        bottomleft[:,:,0] = red
-        bottomleft[:,:,1] = grn
-        bottomleft[:,:,2] = 255 - blu
+        bottomright[:,:,0] = red
+        bottomright[:,:,1] = grn
+        bottomright[:,:,2] = 255 - blu
 
         fgrn = grn.astype(numpy.float32)
         fred = red.astype(numpy.float32)
-        bottomright[:,:,0] = blu
-        bottomright[:,:,1] = (abs(fgrn - fred)).astype(numpy.uint8)
-        bottomright[:,:,2] = red
+        bottomleft[:,:,0] = blu
+        bottomleft[:,:,1] = (abs(fgrn - fred)).astype(numpy.uint8)
+        bottomleft[:,:,2] = red
 
         cv.ShowImage("camera", paste)
         if cv.WaitKey(6) == 27:
             break
-    print "took", time.time() - started
