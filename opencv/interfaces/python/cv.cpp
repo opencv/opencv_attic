cae493b5cf6f15570c6239c2280d900043f3e20d
James Bowman <no@email>
2010-05-07 20:03:19 +0000

    Use SVN Revision
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@3057 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/interfaces/python/cv.cpp
+++ opencv/interfaces/python/cv.cpp
@@ -3851,7 +3851,7 @@ void initcv()
   m = Py_InitModule(MODULESTR"", methods);
   d = PyModule_GetDict(m);
 
-  PyDict_SetItemString(d, "__version__", PyString_FromString("$Id$"));
+  PyDict_SetItemString(d, "__version__", PyString_FromString("$Rev$"));
 
   opencv_error = PyErr_NewException((char*)MODULESTR".error", NULL, NULL);
   PyDict_SetItemString(d, "error", opencv_error);
