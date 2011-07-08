b2431d51c5d7857347a5213578895de4d8de202c
salmanulhaq <no@email>
2011-07-08 19:31:04 +0000

    added one-time program build
    git-svn-id: file://localhost/home/tolik/svnrepo/branches/ocl@5943 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- src/ocl/ocl_util.cpp
+++ src/ocl/ocl_util.cpp
@@ -38,6 +38,9 @@
 
 #include "ocl.hpp"
 
+//forward declaration
+string convertToString(const char *filename);
+
 cl_platform_id cv::ocl::util::GetOCLPlatform()
 {
     cl_platform_id pPlatforms[10] = { 0 };
@@ -69,7 +72,7 @@ int cv::ocl::util::createContext(cl_context* context, cl_command_queue* cmd_queu
 	if(hasGPU)
 		*context = clCreateContextFromType(context_properties, CL_DEVICE_TYPE_GPU, NULL, NULL, NULL);
 	else
-		*context = clCreateContextFromType(context_properties, CL_DEVICE_TYPE_CPU, NULL, NULL, NULL);
+		*context = clCreateContextFromType(context_properties, CL_DEVICE_TYPE_GPU, NULL, NULL, NULL);
 
     // get the list of devices associated with context
     err = clGetContextInfo(*context, CL_CONTEXT_DEVICES, 0, NULL, &cb);
@@ -83,4 +86,117 @@ int cv::ocl::util::createContext(cl_context* context, cl_command_queue* cmd_queu
     free(devices);
 
 	return CL_SUCCESS;
+}
+
+int cv::ocl::util::buildOCLProgram(const char *filename, cl_context* context, cl_command_queue* commandQueue, cl_program* program){
+
+	string  sourceStr = convertToString(filename);
+	cl_int status;
+	cl_device_id  *devices = (cl_device_id *)malloc(4);
+	size_t deviceListSize;
+	const char *source    = sourceStr.c_str();
+	size_t sourceSize[]    = { strlen(source) };
+
+	*program = clCreateProgramWithSource(  *context, 
+              								1, 
+              								&source,
+ 											sourceSize,
+             								&status);
+
+	
+ 	
+
+ 	status = clBuildProgram(*program, 1, devices, NULL, NULL, NULL);
+
+
+	if(status != CL_SUCCESS)
+	{
+    	if(status == CL_BUILD_PROGRAM_FAILURE)
+    	{
+    		cl_int logStatus;
+    		char * buildLog = NULL;
+    		size_t buildLogSize = 0;
+    		logStatus = clGetProgramBuildInfo(	*program,
+                                      			devices[0],
+                                      			CL_PROGRAM_BUILD_LOG,
+                                      			buildLogSize,
+                                      			buildLog,
+												&buildLogSize);
+
+           	if(logStatus != CL_SUCCESS)
+			{
+					 cout<<"Error: Gettin Program build info\n";
+			}
+
+
+            
+        	buildLog = (char*)malloc(buildLogSize);
+
+        	if(buildLog == NULL)
+        	{
+            	 cout<<"Error: Gettin build log\n";
+        	}
+
+        	memset(buildLog, 0, buildLogSize);
+
+        	logStatus = clGetProgramBuildInfo(	*program, 
+												devices[0], 
+												CL_PROGRAM_BUILD_LOG, 
+												buildLogSize, 
+												buildLog, 
+												NULL);
+        	if(logStatus != CL_SUCCESS)
+			{
+			 cout<<"Error: cl_get Program build info failled\n";
+			free(buildLog);
+			}
+
+        	 cout << " \n\t\t\tBUILD LOG\n";
+        	 cout << " ************************************************\n";
+        	 cout << buildLog <<  endl;
+        	 cout << " ************************************************\n";
+        	free(buildLog);
+        }
+
+        if(status != CL_SUCCESS)
+		{
+			 cout<<"Error: cl_buildprogram failed\n";
+		}
+    	
+	}
+	return 1;
+}
+
+string convertToString(const char *filename)
+{
+	size_t size;
+	char*  str;
+	 string s;
+
+	 fstream f(filename, ( fstream::in |  fstream::binary));
+
+	if(f.is_open())
+	{
+		size_t fileSize;
+		f.seekg(0,  fstream::end);
+		size = fileSize = f.tellg();
+		f.seekg(0,  fstream::beg);
+
+		str = new char[size+1];
+		if(!str)
+		{
+			f.close();
+			return NULL;
+		}
+
+		f.read(str, fileSize);
+		f.close();
+		str[size] = '\0';
+	
+		s = str;
+		
+		return s;
+	}
+
+return NULL;
 }
