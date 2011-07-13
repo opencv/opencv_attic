cef74056b89d4f80476f450260024d8393c47d80
salmanulhaq <no@email>
2011-07-13 19:51:57 +0000

    oFlow LK and HS samples of OCL module
    git-svn-id: file://localhost/home/tolik/svnrepo/branches/ocl@6030 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

---
+++ opencv/samples/ocl/opticalflowlk.cpp
@@ -0,0 +1,98 @@
+#include <opencv2/ocl.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv/cv.h>
+
+
+using namespace cv;
+using namespace cv::ocl;
+
+
+//Draws the sample motion vectors on the 1st frame
+void drawOptFlowMap(const Mat& vel, Mat& cflowmap, int step, double, const Scalar& color)
+{
+    for(int y = 0; y < cflowmap.rows; y += step)
+        for(int x = 0; x < cflowmap.cols; x += step)
+        {
+			const Point2f& fxy = vel.at<Point2f>(y, x);
+            line(cflowmap, Point(x,y), Point(cvRound(x+fxy.x), cvRound(y+fxy.y)),color);
+            circle(cflowmap, Point(x,y), 2, color, -1);
+        }
+}
+
+int main(){
+
+	
+	VideoCapture capture(0);
+	if(!capture.isOpened()){
+		cout<<"Failed to capture video, check the video path (or webcam connection) again, exiting...\n";
+		exit(0);
+	}
+
+	const char* winTitle = "Camera feed";
+	Mat frame0color, frame1color;
+
+	namedWindow(winTitle, 1);
+
+	for(;;){
+		//Capture 1st frame
+		capture >> frame0color;
+		if(frame0color.empty()){ cout<<"exiting..."; break; }
+
+		//Create intermediate frames for holding grayscale images of the source frames
+		Mat frame0gray(frame0color.rows, frame0color.cols, CV_8UC1);
+		Mat frame1gray(frame0color.rows, frame0color.cols, CV_8UC1);
+
+		//Capture 2nd frame
+		capture >> frame1color;
+		if(frame1color.empty()){ cout<<"exiting..."; break; }
+
+		//Convert source frames into grayscale
+		cvtColor(frame0color, frame0gray, CV_RGB2GRAY);
+		cvtColor(frame1color, frame1gray, CV_RGB2GRAY);
+
+		
+		//Create horizontal and vertical velocity vectors on CPU and also on GPU
+		Mat velX(frame0gray.rows, frame0gray.cols, CV_32FC1);
+		Mat velY(frame0gray.rows, frame0gray.cols, CV_32FC1);
+		OclMat velXgpu(velX.rows, velX.cols, CV_32FC1);
+		OclMat velYgpu(velX.rows, velX.cols, CV_32FC1);
+
+		CvSize winSize = {3,3};
+
+		//Compute optical flow on the GPU
+		cv::ocl::calcOpticalFlowLK(OclMat(frame0gray), OclMat(frame1gray), velXgpu, velYgpu, winSize);
+
+		//Download velocity vectors to the CPU
+		velXgpu.download(velX);
+		velYgpu.download(velY);
+
+		Mat vel(velX.rows, velX.cols, CV_32FC2);
+		Mat _vel[2];
+		_vel[0] = velX;
+		_vel[1] = velY;
+		merge(_vel, 2, vel);
+
+		for(int i=0;i<1024;i++)
+			cout<<(float)velX.data[i]<<endl;
+
+		//Draw the motion vectors on the source image
+		drawOptFlowMap(vel, frame0color, 8, 1.5, CV_RGB(0, 255, 0));
+
+		imshow(winTitle,frame0color);
+		if(waitKey(30)>=0)
+            break;
+
+		//Cleanup
+		frame0gray.release();
+		frame1gray.release();
+		velX.release();
+		velY.release();
+		velXgpu.release();
+		velYgpu.release();
+		
+	}
+
+		//Some more cleanup
+		frame0color.release();
+		frame1color.release();
+}
