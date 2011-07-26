d7b486329c975093f230078c84f21b5801a8a5c6
Vadim Pisarevsky <no@email>
2011-07-26 15:32:43 +0000

    converted L-K flow sample
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@6207 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv_extra/learning_opencv_v2/ch10_ex10_1.cpp
+++ opencv_extra/learning_opencv_v2/ch10_ex10_1.cpp
@@ -25,99 +25,50 @@
      http://pr.willowgarage.com/wiki/OpenCV
    ************************************************** */
 
-#include <cv.h>
-#include <cxcore.h>
-#include <highgui.h>
-#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
-const int MAX_CORNERS = 500;
+using namespace cv;
+using namespace std;
+
 int main(int argc, char** argv) {
    // Initialize, load two images from the file system, and
    // allocate the images and other structures we will need for
    // results.
 	//
-	IplImage* imgA = cvLoadImage("OpticalFlow0.jpg",CV_LOAD_IMAGE_GRAYSCALE);
-	IplImage* imgB = cvLoadImage("OpticalFlow1.jpg",CV_LOAD_IMAGE_GRAYSCALE);
-	CvSize      img_sz    = cvGetSize( imgA );
-	int         win_size = 10;
-	IplImage* imgC = cvLoadImage("OpticalFlow1.jpg",CV_LOAD_IMAGE_UNCHANGED);
+	Mat imgA = imread("OpticalFlow0.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	Mat imgB = imread("OpticalFlow1.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	Size img_sz = imgA.size();
+	int win_size = 10;
+	Mat imgC = imread("OpticalFlow1.jpg",CV_LOAD_IMAGE_UNCHANGED);
 	
 	// The first thing we need to do is get the features
 	// we want to track.
 	//
-	IplImage* eig_image = cvCreateImage( img_sz, IPL_DEPTH_32F, 1 );
-	IplImage* tmp_image = cvCreateImage( img_sz, IPL_DEPTH_32F, 1 );
-	int              corner_count = MAX_CORNERS;
-	CvPoint2D32f* cornersA        = new CvPoint2D32f[ MAX_CORNERS ];
-	cvGoodFeaturesToTrack(
-		imgA,
-		eig_image,
-		tmp_image,
-		cornersA,
-		&corner_count,
-		0.01,
-		5.0,
-		0,
-		3,
-		0,
-		0.04
-	);
-	cvFindCornerSubPix(
-		imgA,
-		cornersA,
-		corner_count,
-		cvSize(win_size,win_size),
-		cvSize(-1,-1),
-		cvTermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03)
-	);
+    vector<Point2f> cornersA, cornersB;
+    const int MAX_CORNERS = 500;
+	goodFeaturesToTrack(imgA, cornersA, MAX_CORNERS, 0.01, 5, noArray(), 3, false, 0.04);
+    
+	cornerSubPix(imgA, cornersA, Size(win_size, win_size), Size(-1,-1),
+                 TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03));
+
 	// Call the Lucas Kanade algorithm
 	//
-	char features_found[ MAX_CORNERS ];
-	float feature_errors[ MAX_CORNERS ];
-	CvSize pyr_sz = cvSize( imgA->width+8, imgB->height/3 );
-	IplImage* pyrA = cvCreateImage( pyr_sz, IPL_DEPTH_32F, 1 );
-  IplImage* pyrB = cvCreateImage( pyr_sz, IPL_DEPTH_32F, 1 );
-  CvPoint2D32f* cornersB        = new CvPoint2D32f[ MAX_CORNERS ];
-  cvCalcOpticalFlowPyrLK(
-     imgA,
-     imgB,
-     pyrA,
-     pyrB,
-     cornersA,
-     cornersB,
-     corner_count,
-     cvSize( win_size,win_size ),
-     5,
-     features_found,
-     feature_errors,
-     cvTermCriteria( CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, .3 ),
-     0
-  );
-  // Now make some image of what we are looking at:
-  //
-  for( int i=0; i<corner_count; i++ ) {
-     if( features_found[i]==0|| feature_errors[i]>550 ) {
- //       printf("Error is %f/n",feature_errors[i]);
-        continue;
-     }
- //    printf("Got it/n");
-     CvPoint p0 = cvPoint(
-        cvRound( cornersA[i].x ),
-        cvRound( cornersA[i].y )
-     );
-     CvPoint p1 = cvPoint(
-        cvRound( cornersB[i].x ),
-        cvRound( cornersB[i].y )
-     );
-     cvLine( imgC, p0, p1, CV_RGB(255,0,0),2 );
+    vector<uchar> features_found;
+	calcOpticalFlowPyrLK(imgA, imgB, cornersA, cornersB, features_found, noArray(),
+                         Size(win_size*4+1,win_size*4+1), 5,
+                         TermCriteria( CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, .3 ));
+    // Now make some image of what we are looking at:
+    //
+    for( int i = 0; i < (int)cornersA.size(); i++ ) {
+     if( !features_found[i] )
+         continue;
+     line(imgC, cornersA[i], cornersB[i], Scalar(0,255,0), 2, CV_AA);   
   }
-  cvNamedWindow("ImageA",0);
-  cvNamedWindow("ImageB",0);
-  cvNamedWindow("LKpyr_OpticalFlow",0);
-  cvShowImage("ImageA",imgA);
-  cvShowImage("ImageB",imgB);
-  cvShowImage("LKpyr_OpticalFlow",imgC);
-  cvWaitKey(0);
+  imshow("ImageA",imgA);
+  imshow("ImageB",imgB);
+  imshow("LKpyr_OpticalFlow",imgC);
+  waitKey(0);
   return 0;
 }
 
