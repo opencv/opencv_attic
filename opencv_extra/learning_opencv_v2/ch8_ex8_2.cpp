5be169384b2ba621c5d884689ac2bec2335bf4dd
Vadim Pisarevsky <no@email>
2011-07-26 15:19:48 +0000

    converted ch8 samples; removed duplicate
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@6206 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv_extra/learning_opencv_v2/ch8_ex8_2.cpp
+++ opencv_extra/learning_opencv_v2/ch8_ex8_2.cpp
@@ -1,81 +1,41 @@
 //
-// Example 8-2. Finding contours based on a trackbar’s location; the contours are updated
-//              whenever the trackbar is moved
+// example 8-2
 //
 //
-/* *************** License:**************************
-   Oct. 3, 2008
-   Right to use this code in any way you want without warrenty, support or any guarentee of it working.
-
-   BOOK: It would be nice if you cited it:
-   Learning OpenCV: Computer Vision with the OpenCV Library
-     by Gary Bradski and Adrian Kaehler
-     Published by O'Reilly Media, October 3, 2008
- 
-   AVAILABLE AT: 
-     http://www.amazon.com/Learning-OpenCV-Computer-Vision-Library/dp/0596516134
-     Or: http://oreilly.com/catalog/9780596516130/
-     ISBN-10: 0596516134 or: ISBN-13: 978-0596516130    
-
-   OTHER OPENCV SITES:
-   * The source code is on sourceforge at:
-     http://sourceforge.net/projects/opencvlibrary/
-   * The OpenCV wiki page (As of Oct 1, 2008 this is down for changing over servers, but should come back):
-     http://opencvlibrary.sourceforge.net/
-   * An active user group is at:
-     http://tech.groups.yahoo.com/group/OpenCV/
-   * The minutes of weekly OpenCV development meetings are at:
-     http://pr.willowgarage.com/wiki/OpenCV
-   ************************************************** *///
 //
 
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
-#include <cv.h>
-#include <highgui.h>
-#include <stdio.h>
+using namespace cv;
+using namespace std;
 
-IplImage*    g_image    = NULL;
-IplImage*    g_gray    = NULL;
-int        g_thresh  = 100;
-CvMemStorage*  g_storage  = NULL;
+Mat g_gray, g_binary;
+int g_thresh = 100;
 
-void on_trackbar(int) {
-  if( g_storage==NULL ) {
-    g_gray = cvCreateImage( cvGetSize(g_image), 8, 1 );
-    g_storage = cvCreateMemStorage(0);
-  } else {
-    cvClearMemStorage( g_storage );
-  }
-  CvSeq* contours = 0;
-  cvCvtColor( g_image, g_gray, CV_BGR2GRAY );
-  cvThreshold( g_gray, g_gray, g_thresh, 255, CV_THRESH_BINARY );
-  cvFindContours( g_gray, g_storage, &contours );
-  cvZero( g_gray );
-  if( contours )
-    cvDrawContours( 
-      g_gray, 
-      contours, 
-      cvScalarAll(255),
-      cvScalarAll(255), 
-      100 
-    );
-  cvShowImage( "Contours", g_gray );
+void on_trackbar(int, void*) {
+  threshold( g_gray, g_binary, g_thresh, 255, CV_THRESH_BINARY );
+  vector<vector<Point> > contours;
+  findContours(g_binary, contours, noArray(), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+  g_binary = Scalar::all(0);
+    
+  drawContours(g_binary, contours, -1, Scalar::all(255));
+  imshow( "Contours", g_binary );
 }
 
 int main( int argc, char** argv )
 {
-  if( argc != 2 || !(g_image = cvLoadImage(argv[1])) ){
-  printf("\nExample 8_2 Contour retreival using trackbar\nCall is:\n./ch8_ex8_2 image\n");
-  return -1;}
-  cvNamedWindow( "Contours", 1 );
-  cvCreateTrackbar( 
+  if( argc != 2 || (g_gray = imread(argv[1], 0)).empty() )
+      return -1;
+  namedWindow( "Contours", 1 );
+  createTrackbar( 
     "Threshold", 
     "Contours", 
     &g_thresh, 
     255, 
     on_trackbar
   );
-  on_trackbar(0);
-  cvWaitKey();
+  on_trackbar(0, 0);
+  waitKey();
   return 0; 
 }
--- opencv_extra/learning_opencv_v2/ch8_ex8_3.cpp
+++ opencv_extra/learning_opencv_v2/ch8_ex8_3.cpp
@@ -28,73 +28,62 @@
    ************************************************** *///
 //
 
+#include <opencv2/opencv.hpp>
+#include <algorithm>
+#include <iostream>
 
-#include <cv.h>
-#include <highgui.h>
-#include <stdio.h>
+using namespace cv;
+using namespace std;
 
-//Some defines we left out of the book
-#define CVX_RED		CV_RGB(0xff,0x00,0x00)
-#define CVX_GREEN	CV_RGB(0x00,0xff,0x00)
-#define CVX_BLUE	CV_RGB(0x00,0x00,0xff)
+struct AreaCmp
+{
+    AreaCmp(const vector<float>& _areas) : areas(&_areas) {}
+    bool operator()(int a, int b) const { return (*areas)[a] > (*areas)[b]; }
+    const vector<float>* areas;
+};
 
 //  Example 8-3. Finding and drawing contours on an input image
 int main(int argc, char* argv[]) {
 
-  cvNamedWindow( argv[0], 1 );
-  IplImage* img_8uc1 = NULL;
-  
+  Mat img, img_edge, img_color;
   //Changed this a little for safer image loading and help if not
-  if( argc != 2 || !(img_8uc1 = cvLoadImage( argv[1], CV_LOAD_IMAGE_GRAYSCALE )) ){
-  printf("\nExample 8_3 Drawing Contours\nCall is:\n./ch8_ex8_3 image\n\n");
-  return -1;}
-  
-  
-  IplImage* img_edge = cvCreateImage( cvGetSize(img_8uc1), 8, 1 );
-  IplImage* img_8uc3 = cvCreateImage( cvGetSize(img_8uc1), 8, 3 );
-  cvThreshold( img_8uc1, img_edge, 128, 255, CV_THRESH_BINARY );
-  CvMemStorage* storage = cvCreateMemStorage();
-  CvSeq* first_contour = NULL;
-  int Nc = cvFindContours(
-     img_edge,
-     storage,
-     &first_contour,
-     sizeof(CvContour),
-     CV_RETR_LIST // Try all four values and see what happens
-  );
-  int n=0,k;
-  printf("\n\nHit any key to draw the next contour, ESC to quit\n\n");
-  printf( "Total Contours Detected: %d\n", Nc );
-  for( CvSeq* c=first_contour; c!=NULL; c=c->h_next ) {
-     cvCvtColor( img_8uc1, img_8uc3, CV_GRAY2BGR );
-     cvDrawContours(
-        img_8uc3,
-        c,
-        CVX_RED,  //Yarg, these are defined above, but not in the book.  Oops
-        CVX_BLUE,
-        0,        // Try different values of max_level, and see what happens
-        2,
-        8
-     );
-     printf("Contour #%d\n", n );
-     cvShowImage( argv[0], img_8uc3 );
-     printf(" %d elements:\n", c->total );
-     for( int i=0; i<c->total; ++i ) {
-     CvPoint* p = CV_GET_SEQ_ELEM( CvPoint, c, i );
-        printf("    (%d,%d)\n", p->x, p->y );
-     }
-     if((k = cvWaitKey()&0x7F) == 27)
-       break;
-     n++;
+  if( argc != 2 || (img = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE )).empty() ){
+      cout << "\nExample 8_3 Drawing Contours\nCall is:\n./ch8_ex8_3 image\n\n";
+      return -1;
   }
-  printf("Finished all contours. Hit key to finish\n");
-  cvCvtColor( img_8uc1, img_8uc3, CV_GRAY2BGR );
-  cvShowImage( argv[0], img_8uc3 );
-  cvWaitKey(0);
-  cvDestroyWindow( argv[0] );
-  cvReleaseImage( &img_8uc1 );
-  cvReleaseImage( &img_8uc3 );
-  cvReleaseImage( &img_edge );
+  
+  threshold(img, img_edge, 128, 255, THRESH_BINARY);
+  imshow("Image after threshold", img_edge);
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;  
+    
+  findContours(img_edge, contours, hierarchy, RETR_LIST, CHAIN_APPROX_SIMPLE);
+  cout << "\n\nHit any key to draw the next contour, ESC to quit\n\n";
+  cout << "Total Contours Detected: " << contours.size() << endl;
+  vector<int> sortIdx(contours.size());  
+  vector<float> areas(contours.size());
+  for( int n = 0; n < (int)contours.size(); n++ ) {
+      sortIdx[n] = n;
+      areas[n] = contourArea(contours[n], false);
+  }
+  // sort contours so that largest contours go first
+  std::sort(sortIdx.begin(), sortIdx.end(), AreaCmp(areas));
+    
+  for( int n = 0; n < (int)sortIdx.size(); n++ ) {
+     int idx = sortIdx[n]; 
+     cvtColor( img, img_color, CV_GRAY2BGR );
+     drawContours(img_color, contours, idx,
+                   Scalar(0,0,255), 2, 8, hierarchy,
+                   0 // Try different values of max_level, and see what happens
+                   ); 
+     cout << "Contour #" << idx << ": area=" << areas[idx] <<
+        ", nvertices=" << contours[idx].size() << endl;
+     imshow(argv[0], img_color);
+     int k;
+     if((k = waitKey()&255) == 27)
+       break;
+  }
+  cout << "Finished all contours\n";
   return 0;
 }
 
