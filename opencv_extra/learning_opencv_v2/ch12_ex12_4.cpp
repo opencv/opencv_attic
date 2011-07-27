63cd9eaea5a4f3b6008eb59e3df5c0aca4e6f98a
Vadim Pisarevsky <no@email>
2011-07-27 20:01:50 +0000

    started ch12
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@6230 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv_extra/learning_opencv_v2/ch12_ex12_4.cpp
+++ opencv_extra/learning_opencv_v2/ch12_ex12_4.cpp
@@ -25,78 +25,72 @@
      http://pr.willowgarage.com/wiki/OpenCV
    ************************************************** */
 
-#include "cv.h"
-#include "highgui.h"
+#include "opencv2/opencv.hpp"
 #include <math.h>
 
+using namespace cv;
 
 int main( int argc, char** argv )
 {
-    IplImage* img = cvCreateImage( cvSize( 500, 500 ), 8, 3 );
-    CvRNG rng = cvRNG(-1);
+    Mat img(500, 500, CV_8UC3);
+    RNG rng(-1);
 
-    cvNamedWindow( "fitline", 1 );
-        
     for(;;)
     {
         char key;
-        int i, count = cvRandInt(&rng)%100 + 1, outliers = count/5;
-        float a = cvRandReal(&rng)*200;
-        float b = cvRandReal(&rng)*40;
-        float angle = cvRandReal(&rng)*CV_PI;
+        int i, count = rng.uniform(0,100) + 3, outliers = count/5;
+        float a = (float)rng.uniform(0., 200.);
+        float b = (float)rng.uniform(0., 40.);
+        float angle = (float)rng.uniform(0., CV_PI);
         float cos_a = cos(angle), sin_a = sin(angle);
-        CvPoint pt1, pt2;
-        CvPoint* points = (CvPoint*)malloc( count * sizeof(points[0]));
-        CvMat pointMat = cvMat( 1, count, CV_32SC2, points );
-        float line[4];
+        Point pt1, pt2;
+        vector<Point> points(count);
+        Vec4f line;
         float d, t;
 
-        b = MIN(a*0.3, b);
+        b = MIN(a*0.3f, b);
 
         // generate some points that are close to the line
         for( i = 0; i < count - outliers; i++ )
         {
-            float x = (cvRandReal(&rng)*2-1)*a;
-            float y = (cvRandReal(&rng)*2-1)*b;
-            points[i].x = cvRound(x*cos_a - y*sin_a + img->width/2);
-            points[i].y = cvRound(x*sin_a + y*cos_a + img->height/2);
+            float x = (float)rng.uniform(-1.,1.)*a;
+            float y = (float)rng.uniform(-1.,1.)*b;
+            points[i].x = cvRound(x*cos_a - y*sin_a + img.cols/2);
+            points[i].y = cvRound(x*sin_a + y*cos_a + img.rows/2);
         }
 
         // generate "completely off" points
         for( ; i < count; i++ )
         {
-            points[i].x = cvRandInt(&rng) % img->width;
-            points[i].y = cvRandInt(&rng) % img->height;
+            points[i].x = rng.uniform(0, img.cols);
+            points[i].y = rng.uniform(0, img.rows);
         }
 
         // find the optimal line
-        cvFitLine( &pointMat, CV_DIST_L1, 1, 0.001, 0.001, line );
-        cvZero( img );
+        fitLine( points, line, CV_DIST_L1, 1, 0.001, 0.001);
 
         // draw the points
+        img = Scalar::all(0);
         for( i = 0; i < count; i++ )
-            cvCircle( img, points[i], 2, i < count - outliers ? CV_RGB(255, 0, 0) :
-                CV_RGB(255,255,0), CV_FILLED, CV_AA, 0 );
+            circle( img, points[i], 2, i < count - outliers ? Scalar(0, 0, 255) :
+                Scalar(0,255,255), CV_FILLED, CV_AA, 0 );
 
         // ... and the long enough line to cross the whole image
         d = sqrt((double)line[0]*line[0] + (double)line[1]*line[1]);
         line[0] /= d;
         line[1] /= d;
-        t = (float)(img->width + img->height);
+        t = (float)(img.cols + img.rows);
         pt1.x = cvRound(line[2] - line[0]*t);
         pt1.y = cvRound(line[3] - line[1]*t);
         pt2.x = cvRound(line[2] + line[0]*t);
         pt2.y = cvRound(line[3] + line[1]*t);
-        cvLine( img, pt1, pt2, CV_RGB(0,255,0), 3, CV_AA, 0 );
+        cv::line( img, pt1, pt2, Scalar(0,255,0), 3, CV_AA, 0 );
 
-        cvShowImage( "fitline", img );
+        imshow( "Fit Line", img );
 
-        key = (char) cvWaitKey(0);
+        key = (char)waitKey(0);
         if( key == 27 || key == 'q' || key == 'Q' ) // 'ESC'
             break;
-        free( points );
     }
-    
-    cvDestroyWindow( "fitline" );
     return 0;
 }
