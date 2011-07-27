6b51da429e582332daa7a09d17ace98d0c3ddef6
Gary Bradski <no@email>
2011-07-27 23:34:37 +0000

    some headers and help functios
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@6235 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv_extra/learning_opencv_v2/ch11_ex11_1_fromdisk.cpp
+++ opencv_extra/learning_opencv_v2/ch11_ex11_1_fromdisk.cpp
@@ -1,35 +1,33 @@
 // Calibrate from a list of images
 //
 // console application.
-// Gary Bradski April'08
+// Gary Bradski April'08, converted to C++ by Vadim Pisarevsky June'11.
 // 
-/* *************** License:**************************
-   Oct. 3, 2008
-   Right to use this code in any way you want without warrenty, support or any guarentee of it working.
+/* License:
+   July 20, 2011
+   Standard BSD
 
    BOOK: It would be nice if you cited it:
-   Learning OpenCV: Computer Vision with the OpenCV Library
+   Learning OpenCV 2: Computer Vision with the OpenCV Library
      by Gary Bradski and Adrian Kaehler
-     Published by O'Reilly Media, October 3, 2008
+     Published by O'Reilly Media
  
    AVAILABLE AT: 
      http://www.amazon.com/Learning-OpenCV-Computer-Vision-Library/dp/0596516134
      Or: http://oreilly.com/catalog/9780596516130/
      ISBN-10: 0596516134 or: ISBN-13: 978-0596516130    
 
-   OTHER OPENCV SITES:
-   * The source code is on sourceforge at:
-     http://sourceforge.net/projects/opencvlibrary/
-   * The OpenCV wiki page (As of Oct 1, 2008 this is down for changing over servers, but should come back):
-     http://opencvlibrary.sourceforge.net/
+   Main OpenCV site
+   http://opencv.willowgarage.com/wiki/
    * An active user group is at:
      http://tech.groups.yahoo.com/group/OpenCV/
    * The minutes of weekly OpenCV development meetings are at:
      http://pr.willowgarage.com/wiki/OpenCV
-   ************************************************** */
+*/
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
