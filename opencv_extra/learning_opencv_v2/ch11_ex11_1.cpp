cb5d086966ef0f852800c51f17aaae7939c2ad62
Gary Bradski <no@email>
2011-07-27 17:59:44 +0000

    ch11_ex11_1 header
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@6227 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv_extra/learning_opencv_v2/ch11_ex11_1.cpp
+++ opencv_extra/learning_opencv_v2/ch11_ex11_1.cpp
@@ -23,30 +23,27 @@ void help(){
     "\n";
 }
 //
-/* *************** License:**************************
- Oct. 3, 2008
- Right to use this code in any way you want without warrenty, support or any guarentee of it working.
+/* License:
+   July 20, 2011
+   Standard BSD
+
+   BOOK: It would be nice if you cited it:
+   Learning OpenCV 2: Computer Vision with the OpenCV Library
+     by Gary Bradski and Adrian Kaehler
+     Published by O'Reilly Media
  
- BOOK: It would be nice if you cited it:
- Learning OpenCV: Computer Vision with the OpenCV Library
- by Gary Bradski and Adrian Kaehler
- Published by O'Reilly Media, October 3, 2008
- 
- AVAILABLE AT: 
- http://www.amazon.com/Learning-OpenCV-Computer-Vision-Library/dp/0596516134
- Or: http://oreilly.com/catalog/9780596516130/
- ISBN-10: 0596516134 or: ISBN-13: 978-0596516130    
- 
- OTHER OPENCV SITES:
- * The source code is on sourceforge at:
- http://sourceforge.net/projects/opencvlibrary/
- * The OpenCV wiki page (As of Oct 1, 2008 this is down for changing over servers, but should come back):
- http://opencvlibrary.sourceforge.net/
- * An active user group is at:
- http://tech.groups.yahoo.com/group/OpenCV/
- * The minutes of weekly OpenCV development meetings are at:
- http://pr.willowgarage.com/wiki/OpenCV
- ************************************************** */
+   AVAILABLE AT:
+     http://www.amazon.com/Learning-OpenCV-Computer-Vision-Library/dp/0596516134
+     Or: http://oreilly.com/catalog/9780596516130/
+     ISBN-10: 0596516134 or: ISBN-13: 978-0596516130
+
+   Main OpenCV site
+   http://opencv.willowgarage.com/wiki/
+   * An active user group is at:
+     http://tech.groups.yahoo.com/group/OpenCV/
+   * The minutes of weekly OpenCV development meetings are at:
+     http://pr.willowgarage.com/wiki/OpenCV
+*/
 //
 
 int main(int argc, char* argv[]) {
