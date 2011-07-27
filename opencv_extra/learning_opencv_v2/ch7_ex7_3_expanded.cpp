d2e46bc4453032b33ece84c3d65135e26458b6d4
Gary Bradski <no@email>
2011-07-27 23:18:34 +0000

    ch7_ex7_3_expanded
    git-svn-id: file://localhost/home/tolik/svnrepo/trunk@6234 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv_extra/learning_opencv_v2/ch7_ex7_3_expanded.cpp
+++ opencv_extra/learning_opencv_v2/ch7_ex7_3_expanded.cpp
@@ -1,219 +1,181 @@
-#include <cv.h>
-#include <cxcore.h>
-#include <highgui.h>
-#include <stdio.h>
-/* *************** License:**************************
-   Oct. 3, 2008
-   Right to use this code in any way you want without warrenty, support or any guarentee of it working.
+/* License:
+   July 20, 2011
+   Standard BSD
 
    BOOK: It would be nice if you cited it:
-   Learning OpenCV: Computer Vision with the OpenCV Library
+   Learning OpenCV 2: Computer Vision with the OpenCV Library
      by Gary Bradski and Adrian Kaehler
-     Published by O'Reilly Media, October 3, 2008
+     Published by O'Reilly Media
  
    AVAILABLE AT: 
      http://www.amazon.com/Learning-OpenCV-Computer-Vision-Library/dp/0596516134
      Or: http://oreilly.com/catalog/9780596516130/
      ISBN-10: 0596516134 or: ISBN-13: 978-0596516130    
 
-   OTHER OPENCV SITES:
-   * The source code is on sourceforge at:
-     http://sourceforge.net/projects/opencvlibrary/
-   * The OpenCV wiki page (As of Oct 1, 2008 this is down for changing over servers, but should come back):
-     http://opencvlibrary.sourceforge.net/
+   Main OpenCV site
+   http://opencv.willowgarage.com/wiki/
    * An active user group is at:
      http://tech.groups.yahoo.com/group/OpenCV/
    * The minutes of weekly OpenCV development meetings are at:
      http://pr.willowgarage.com/wiki/OpenCV
-   ************************************************** */
+*/
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
 
 void help(){
-printf("\nCall is:\n" 
-		"ch7_ex7_3_expanded modelImage0 testImage1 testImage2 badImage3\n\n");
-printf(
-"  Note that the model image is split in half.  Top half(0) makes model.  It's then tested\n"
-"  against its lower half(0), testImages 1 and 2 in different lighting and different object 3\n\n");
+cout << "\nCall is:\n" <<
+		"./ch7_ex7_3_expanded modelImage0 testImage1 testImage2 badImage3\n\n" <<
+		"for example: ./ch7_ex7_3_expanded HandIndoorColor.jpg HandOutdoorColor.jpg HandOutdoorSunColor.jpg fruits.jpg\n\n" <<
+"  Note that the model image is split in half.  Top half(0) makes model.  It's then tested\n" <<
+"  against its lower half(0), testImages 1 and 2 in different lighting and different object 3\n\n" << endl;
 }
 //Compare 3 images histograms together, 
 // the first is divided in half along y to test its other half
 // Call is: 
-//    ch7HistCmp modelImage0 testImage1 testImage2 badImage3
+//    ./ch7_ex7_3_expanded modelImage0 testImage1 testImage2 badImage3
 // Note that the model image is split in half.  Top half(0) makes model.  It's then tested
 // against its lower half(0), testImages 1 and 2 in different lighting and different object 3
 // 
 int main( int argc, char** argv ) {
 
-    IplImage* src[5], *tmp;
+	vector<Mat> src(5);
+	Mat tmp;
 	int i;
-    if( argc == 5){ 
-		if((tmp = cvLoadImage(argv[1], 1)) == 0){ //We're going to split this one in half
-			printf("Error on reading image 1, %s\n",argv[1]);
+	if(argc != 5) { help(); return -1;}
+
+	tmp = imread(argv[1], 1);
+	if(tmp.empty()){ //We're going to split this one in half
+		cerr << "Error on reading image 1," << argv[1] << "\n" << endl;
+		help();
+		return(-1);
+	}
+
+	//Parse the first image into two image halves divided halfway on y
+	cout << "Getting size [[" << tmp.cols << "] [" << tmp.rows << "]]\n" << endl;
+	Size size = tmp.size();
+	cout << "Got size (w,h): (" << size.width << "," << size.height << ")" << endl;
+	int width = size.width;
+	int height = size.height;
+	int halfheight = height >> 1;
+	src[0] = Mat(Size(width,halfheight), CV_8UC3);
+	src[1] = Mat(Size(width,halfheight), CV_8UC3);
+
+
+	//Divide the first image into top and bottom halfs in src[0] and src[1] respectively
+	Mat_<Vec3b>::iterator tmpit = tmp.begin<Vec3b>();
+	Mat_<Vec3b>::iterator s0it = src[0].begin<Vec3b>();
+
+	for(i = 0; i < width*halfheight; ++i, ++tmpit, ++s0it) //top half
+		*s0it = *tmpit;
+	Mat_<Vec3b>::iterator s1it = src[1].begin<Vec3b>();
+	for(i = 0; i < width*halfheight; ++i, ++tmpit, ++s1it) //Bottom half
+		*s1it = *tmpit;
+
+	//LOAD THE OTHER THREE IMAGES
+	for(i = 2; i<5; ++i){
+		src[i] = imread(argv[i], 1);
+		if(src[i].empty())
+		{
+			cerr << "Error on reading image " << i << ": " << argv[i] << "\n" << endl;
 			help();
 			return(-1);
 		}
-		//Parse the first image into two image halves divided halfway on y
-		printf("Getting size [[%d] [%d]]\n",tmp->width,tmp->height);
-		CvSize size = cvGetSize(tmp);
-		printf("Get size %d %d\n",size.width,size.height);
-		int width = size.width;
-		int height = size.height;
-		int halfheight = height >> 1;
-		src[0] = cvCreateImage(cvSize(width,halfheight), 8, 3);
-		src[1] = cvCreateImage(cvSize(width,halfheight), 8, 3);
-		if(src[0]->widthStep != tmp->widthStep){
-			printf("Error, Withstep of alloated src doesn't equal withStep of loaded image [%d vs %d]",
-					src[0]->widthStep,tmp->widthStep);
-			help();
-			return(-2);
-		}
-		int widthStep = tmp->widthStep;
-		unsigned char *p = (unsigned char*)(tmp->imageData);
-		unsigned char *sp = (unsigned char*)(src[0]->imageData);
-		for(i=0; i<widthStep*halfheight; ++i){//Top half
-			*sp++ = *p++;
-		}
-		sp = (unsigned char*)(src[1]->imageData);
-		for(i=0; i<widthStep*halfheight; ++i) {//Bottom half
-			*sp++ = *p++;
-		}
-		//LOAD THE OTHER THREE IMAGES
-		for(i = 2; i<5; ++i){
-			if((src[i]=cvLoadImage(argv[i], 1))== 0) {
-				printf("Error on reading image %d: %s\n",i,argv[i]);
-				help();
-				return(-1);
+	}
+
+
+	// Compute the HSV image, and decompose it into separate planes.
+	//
+
+
+
+	vector<Mat> hsv(5),hist(5),hist_img(5);
+	int h_bins = 8, s_bins = 8;
+	int    hist_size[] = { h_bins, s_bins }, ch[] = {0, 1};
+	float  h_ranges[]  = { 0, 180 };          // hue is [0,180]
+	float  s_ranges[]  = { 0, 255 };
+	const float* ranges[]    = { h_ranges, s_ranges };
+	int scale = 10;
+
+	for(i = 0; i<5; ++i){
+		cvtColor( src[i], hsv[i], CV_BGR2HSV );
+		calcHist(&hsv[i],1,ch,noArray(),hist[i],2,hist_size,ranges,true);
+		normalize( hist[i], hist[i], 0, 255, NORM_MINMAX);
+		hist_img[i] = Mat::zeros(hist_size[0]*scale,hist_size[1]*scale,CV_8UC3);
+		//Draw our histogram
+		for( int h = 0; h < hist_size[0]; h++ )
+			for( int s = 0; s < hist_size[1]; s++ )
+			{
+				float hval = hist[i].at<float>(h, s);
+				rectangle(hist_img[i], Rect(h*scale, s*scale, scale, scale),
+						Scalar::all(hval), -1);
 			}
+	}//For the 5 images
+
+	//DISPLAY
+	namedWindow( "Source0", 1 );
+	imshow(   "Source0", src[0] );
+	namedWindow( "H-S Histogram0", 1 );
+	imshow(   "H-S Histogram0", hist_img[0] );
+
+	namedWindow( "Source1", 1 );
+	imshow(   "Source1", src[1] );
+	namedWindow( "H-S Histogram1", 1 );
+	imshow(   "H-S Histogram1", hist_img[1] );
+
+	namedWindow( "Source2", 1 );
+	imshow(   "Source2", src[2] );
+	namedWindow( "H-S Histogram2", 1 );
+	imshow(   "H-S Histogram2", hist_img[2] );
+
+	namedWindow( "Source3", 1 );
+	imshow(   "Source3", src[3] );
+	namedWindow( "H-S Histogram3", 1 );
+	imshow(   "H-S Histogram3", hist_img[3] );
+
+	namedWindow( "Source4", 1 );
+	imshow(   "Source4", src[4] );
+	namedWindow( "H-S Histogram4", 1 );
+	imshow(   "H-S Histogram4", hist_img[4] );
+
+	//Compare the histogram src0 vs 1, vs 2, vs 3, vs 4
+	cout << "Comparison:\nCorr                 Chi                 Intersect          Bhat\n" << endl;
+	for(i=1; i<5; ++i){//For histogram
+		cout << "Hist[0] vs: Hist["<<i<<"]: " << endl;;
+		for(int j=0; j<4; ++j) { //For comparision type
+			cout << "method["<<j<<"]: " << compareHist(hist[0],hist[i],j) << "  ";
 		}
+		cout << endl;
+	}
+//	//Oi Vey, parse histogram to earth movers signatures
+//	//		CvRNG rng_state = cvRNG(0xffffffff); //Tested random bins
+//	CvMat* sig[5];
+//	int numrows = h_bins*s_bins;
+//	int numcols = 3; //value,i,j
+//	for(i=0; i<5; ++i){
+//		sig[i] = cvCreateMat(numrows, 3, CV_32FC1);
+//		//fill it
+//		float sum = 0.0;
+//		for( int h = 0; h < h_bins; h++ ) {
+//			for( int s = 0; s < s_bins; s++ ) {
+//				float bin_val = cvQueryHistValue_2D( hist[i], h, s );
+//				cvSet2D(sig[i],h*s_bins + s,0,cvScalar(bin_val,bin_val,bin_val)); //Point weight
+//				cvSet2D(sig[i],h*s_bins + s,1,cvScalar(h)); //Coord 1
+//				cvSet2D(sig[i],h*s_bins + s,2,cvScalar(s)); //Coord 2
+//			}
+//		}
+//	}
+//	//Do EMD AND REPORT
+//	printf("EMD: ");
+//	for(i=1; i<5; ++i){
+//		float emd = cvCalcEMD2(sig[0],sig[i],CV_DIST_L2);
+//		printf("%f; \n",emd);
+//	}
+//	printf("\n");
 
-        // Compute the HSV image, and decompose it into separate planes.
-        //
-        IplImage *hsv[5], *h_plane[5],*s_plane[5],*v_plane[5],*planes[5][2]; 
-       	IplImage* hist_img[5];
-		CvHistogram* hist[5];
-	    int h_bins = 8, s_bins = 8;
-        int    hist_size[] = { h_bins, s_bins };
-        float  h_ranges[]  = { 0, 180 };          // hue is [0,180]
-        float  s_ranges[]  = { 0, 255 }; 
-        float* ranges[]    = { h_ranges, s_ranges };
-		int scale = 10;
- 		for(i = 0; i<5; ++i){ 
-			hsv[i] = cvCreateImage( cvGetSize(src[i]), 8, 3 ); 
-        	cvCvtColor( src[i], hsv[i], CV_BGR2HSV );
+	waitKey(0);
 
-			h_plane[i]  = cvCreateImage( cvGetSize(src[i]), 8, 1 );
-        	s_plane[i]  = cvCreateImage( cvGetSize(src[i]), 8, 1 );
-        	v_plane[i]  = cvCreateImage( cvGetSize(src[i]), 8, 1 );
-        	planes[i][0] = h_plane[i];
-			planes[i][1] = s_plane[i];
-        	cvCvtPixToPlane( hsv[i], h_plane[i], s_plane[i], v_plane[i], 0 );
-	        // Build the histogram and compute its contents.
-    	    //
-            {
-         		hist[i] = cvCreateHist( 
-            	2, 
-            	hist_size, 
-            	CV_HIST_ARRAY, 
-            	ranges, 
-            	1 
-          		); 
-        	}	
-        	cvCalcHist( planes[i], hist[i], 0, 0 );
-			cvNormalizeHist( hist[i], 1.0 );
-
-
-
-			// Create an image to use to visualize our histogram.
-  	        //
-         	hist_img[i] = cvCreateImage(  
-          		cvSize( h_bins * scale, s_bins * scale ), 
-          		8, 
-          		3
-        	); 
-        	cvZero( hist_img[i] );
-
-        	// populate our visualization with little gray squares.
-        	//
-        	float max_value = 0;
-			float *fp,fval;
-        	cvGetMinMaxHistValue( hist[i], 0, &max_value, 0, 0 );
-
-	        for( int h = 0; h < h_bins; h++ ) {
-    	        for( int s = 0; s < s_bins; s++ ) {
-        	        float bin_val = cvQueryHistValue_2D( hist[i], h, s );
-	               	int intensity = cvRound( bin_val * 255 / max_value );
-                	cvRectangle( 
-                  		hist_img[i], 
-                  		cvPoint( h*scale, s*scale ),
-                  		cvPoint( (h+1)*scale - 1, (s+1)*scale - 1),
-                  		CV_RGB(intensity,intensity,intensity), 
-                  		CV_FILLED
-                	);
-            	}
-        	}
-		}//For the 5 images
-
-        //DISPLAY
-		cvNamedWindow( "Source0", 1 );
-        cvShowImage(   "Source0", src[0] );
-        cvNamedWindow( "H-S Histogram0", 1 );
-        cvShowImage(   "H-S Histogram0", hist_img[0] );
-
-		cvNamedWindow( "Source1", 1 );
-        cvShowImage(   "Source1", src[1] );
-        cvNamedWindow( "H-S Histogram1", 1 );
-        cvShowImage(   "H-S Histogram1", hist_img[1] );
-
-		cvNamedWindow( "Source2", 1 );
-        cvShowImage(   "Source2", src[2] );
-        cvNamedWindow( "H-S Histogram2", 1 );
-        cvShowImage(   "H-S Histogram2", hist_img[2] );
-
-		cvNamedWindow( "Source3", 1 );
-        cvShowImage(   "Source3", src[3] );
-        cvNamedWindow( "H-S Histogram3", 1 );
-        cvShowImage(   "H-S Histogram3", hist_img[3] );
-
-		cvNamedWindow( "Source4", 1 );
-        cvShowImage(   "Source4", src[4] );
-        cvNamedWindow( "H-S Histogram4", 1 );
-        cvShowImage(   "H-S Histogram4", hist_img[4] );
-
-		//Compare the histogram src0 vs 1, vs 2, vs 3, vs 4
-		printf("Comparison                         Corr                    Chi                     Intersect               Bhat\n");
-		for(i=1; i<5; ++i){//For histogram 
-			printf("Hist[0] vs: Hist[%d]: ",i);
-			for(int j=0; j<4; ++j) { //For comparision type
-				printf("CmpMethod[%d]: %lf; ",j,cvCompareHist(hist[0],hist[i],j));
-			}
-			printf("\n");
-		}
-		//Oi Vey, parse histogram to earth movers signatures
-//		CvRNG rng_state = cvRNG(0xffffffff); //Tested random bins
-		CvMat* sig[5];
-		int numrows = h_bins*s_bins;
-		int numcols = 3; //value,i,j
-		for(i=0; i<5; ++i){
-			sig[i] = cvCreateMat(numrows, 3, CV_32FC1);
-			//fill it
-			float sum = 0.0;
-	        for( int h = 0; h < h_bins; h++ ) {
-    	        for( int s = 0; s < s_bins; s++ ) {
-        	        float bin_val = cvQueryHistValue_2D( hist[i], h, s );
-					cvSet2D(sig[i],h*s_bins + s,0,cvScalar(bin_val,bin_val,bin_val)); //Point weight
-					cvSet2D(sig[i],h*s_bins + s,1,cvScalar(h)); //Coord 1
-					cvSet2D(sig[i],h*s_bins + s,2,cvScalar(s)); //Coord 2
-				}
-			}
-		}
-		//Do EMD AND REPORT
-		printf("EMD: ");
-		for(i=1; i<5; ++i){
-			float emd = cvCalcEMD2(sig[0],sig[i],CV_DIST_L2);
-			printf("%f; \n",emd);
-		}
-		printf("\n");
-	
-        cvWaitKey(0);
-    }
-	else { printf("Error: Wrong number of arguments\n"); help();}
 }
