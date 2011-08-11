6c8707e0549e12d71cc20b1c93d6a97b4e17f1bc
Andrey Kamaev <no@email>
2011-08-11 12:47:01 +0000

    cmake is updated to build performance tests
    git-svn-id: file://localhost/home/tolik/svnrepo/branches/perf_tests@6431 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

---
+++ modules/core/perf/perf_main.cpp
@@ -0,0 +1,3 @@
+#include "perf_precomp.hpp"
+
+CV_TEST_MAIN("cv")
---
+++ modules/core/perf/perf_precomp.hpp
@@ -0,0 +1,7 @@
+#ifndef __OPENCV_PERF_PRECOMP_HPP__
+#define __OPENCV_PERF_PRECOMP_HPP__
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/ts/ts.hpp"
+
+#endif
