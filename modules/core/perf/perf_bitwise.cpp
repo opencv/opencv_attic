210d6ddf5382f82526f394d7138eb1c8656952ea
Leonid Beynenson <no@email>
2011-08-30 14:56:01 +0000

    Added tests for OpenCV bitwise matrix operation
    git-svn-id: file://localhost/home/tolik/svnrepo/branches/perf_tests@6607 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

---
+++ modules/core/perf/perf_bitwise.cpp
@@ -0,0 +1,155 @@
+#include "perf_precomp.hpp"
+
+using namespace cv;
+typedef perf::TestBaseWithParam<perf::iMat> math;
+
+PERF_TEST_P(math, bitwise_not, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
+                                                  mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
+                                                  mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
+                                                  mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
+                                                  mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
+                                                  mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
+                                                  mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
+                                                  mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
+                                                  mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
+    cv::Mat a = GetParam().makeMat(-20, 21, -22, 23);
+    cv::Mat c = GetParam().makeMat();
+
+    declare.in(a, WARMUP_RNG)
+        .out(c)
+        .time(0.5);
+
+    TEST_CYCLE(100) cv::bitwise_not(a, c);
+
+    SANITY_CHECK(c);
+}
+
+PERF_TEST_P(math, bitwise_and, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
+                                                  mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
+                                                  mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
+                                                  mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
+                                                  mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
+                                                  mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
+                                                  mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
+                                                  mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
+                                                  mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
+    cv::Mat a = GetParam().makeMat(-20, 21, -22, 23);
+    cv::Mat b = GetParam().makeMat(-16, 10, 8, 14);
+    cv::Mat c = GetParam().makeMat();
+
+    declare.in(a, b, WARMUP_RNG)
+        .out(c)
+        .time(0.5);
+
+    TEST_CYCLE(100) cv::bitwise_and(a,b, c);
+
+    SANITY_CHECK(c);
+}
+
+PERF_TEST_P(math, bitwise_or, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
+                                                 mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
+                                                 mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
+                                                 mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
+                                                 mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
+                                                 mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
+                                                 mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
+                                                 mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
+                                                 mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
+    cv::Mat a = GetParam().makeMat(-20, 21, -22, 23);
+    cv::Mat b = GetParam().makeMat(-16, 10, 8, 14);
+    cv::Mat c = GetParam().makeMat();
+
+    declare.in(a, b, WARMUP_RNG)
+        .out(c)
+        .time(0.5);
+
+    TEST_CYCLE(100) cv::bitwise_or(a,b, c);
+
+    SANITY_CHECK(c);
+}
+PERF_TEST_P(math, bitwise_xor, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
+                                                  mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
+                                                  mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
+                                                  mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
+                                                  mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
+                                                  mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
+                                                  mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
+                                                  mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
+                                                  mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
+    cv::Mat a = GetParam().makeMat(-20, 21, -22, 23);
+    cv::Mat b = GetParam().makeMat(-16, 10, 8, 14);
+    cv::Mat c = GetParam().makeMat();
+
+    declare.in(a, b, WARMUP_RNG)
+        .out(c)
+        .time(0.5);
+
+    TEST_CYCLE(100) cv::bitwise_xor(a,b, c);
+
+    SANITY_CHECK(c);
+}
+
+PERF_TEST_P(math, bitwise_and__Scalar, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
+                                                          mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
+                                                          mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
+                                                          mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
+                                                          mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
+                                                          mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
+                                                          mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
+                                                          mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
+                                                          mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
+    cv::Mat a = GetParam().makeMat(-20, 21, -22, 23);
+    cv::Scalar b;
+    cv::Mat c = GetParam().makeMat();
+
+    declare.in(a, b, WARMUP_RNG)
+        .out(c)
+        .time(0.5);
+
+    TEST_CYCLE(100) cv::bitwise_and(a,(InputArray)b, c);
+
+    SANITY_CHECK(c);
+}
+
+PERF_TEST_P(math, bitwise_or__Scalar, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
+                                                         mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
+                                                         mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
+                                                         mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
+                                                         mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
+                                                         mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
+                                                         mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
+                                                         mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
+                                                         mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
+    cv::Mat a = GetParam().makeMat(-20, 21, -22, 23);
+    cv::Scalar b;
+    cv::Mat c = GetParam().makeMat();
+
+    declare.in(a, b, WARMUP_RNG)
+        .out(c)
+        .time(0.5);
+
+    TEST_CYCLE(100) cv::bitwise_or(a,(InputArray)b, c);
+
+    SANITY_CHECK(c);
+}
+PERF_TEST_P(math, bitwise_xor__Scalar, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
+                                                          mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
+                                                          mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
+                                                          mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
+                                                          mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
+                                                          mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
+                                                          mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
+                                                          mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
+                                                          mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
+    cv::Mat a = GetParam().makeMat(-20, 21, -22, 23);
+    cv::Scalar b;
+    cv::Mat c = GetParam().makeMat();
+
+    declare.in(a, b, WARMUP_RNG)
+        .out(c)
+        .time(0.5);
+
+    TEST_CYCLE(100) cv::bitwise_xor(a,(InputArray)b, c);
+
+    SANITY_CHECK(c);
+}
--- modules/core/perf/perf_minmax.cpp
+++ modules/core/perf/perf_minmax.cpp
@@ -47,15 +47,15 @@ PERF_TEST_P(math, max, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), m
     SANITY_CHECK(c);
 }
 
-PERF_TEST_P(math, minS, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
-                                           mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
-                                           mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
-                                           mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
-                                           mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
-                                           mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
-                                           mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
-                                           mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
-                                           mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
+PERF_TEST_P(math, min__Scalar, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
+                                                  mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
+                                                  mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
+                                                  mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
+                                                  mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
+                                                  mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
+                                                  mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
+                                                  mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
+                                                  mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
     cv::Mat a = GetParam().makeMat(-20, 21, -22, 23);
     cv::Scalar b;
     cv::Mat c = GetParam().makeMat();
@@ -69,15 +69,15 @@ PERF_TEST_P(math, minS, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), 
     SANITY_CHECK(c);
 }
 
-PERF_TEST_P(math, maxS, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
-                                           mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
-                                           mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
-                                           mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
-                                           mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
-                                           mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
-                                           mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
-                                           mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
-                                           mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
+PERF_TEST_P(math, max__Scalar, ::testing::Values( mVGA8SC1(), mqHD8SC1(), m720p8SC1(), mODD8SC1(), 
+                                                  mVGA8SC3(), mqHD8SC3(), m720p8SC3(), mODD8SC3(), 
+                                                  mVGA8SC4(), mqHD8SC4(), m720p8SC4(), mODD8SC4(),
+                                                  mVGA32SC1(), mqHD32SC1(), m720p32SC1(), mODD32SC1(), 
+                                                  mVGA32SC3(), mqHD32SC3(), m720p32SC3(), mODD32SC3(), 
+                                                  mVGA32SC4(), mqHD32SC4(), m720p32SC4(), mODD32SC4(),
+                                                  mVGA32FC1(), mqHD32FC1(), m720p32FC1(), mODD32FC1(), 
+                                                  mVGA32FC3(), mqHD32FC3(), m720p32FC3(), mODD32FC3(), 
+                                                  mVGA32FC4(), mqHD32FC4(), m720p32FC4(), mODD32FC4() )){
     cv::Mat a = GetParam().makeMat(-20, 21, -22, 23);
     cv::Scalar b;
     cv::Mat c = GetParam().makeMat();
