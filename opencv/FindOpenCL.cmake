8597a53cca1c541fca14c7efebc07377d4902df0
Yao Wang <no@email>
2012-02-27 08:07:49 +0000

    fix bug of finding OpenCL lib in AMDAPPSDK on Windows
    git-svn-id: file://localhost/home/tolik/svnrepo/branches/ocl@7397 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv/FindOpenCL.cmake
+++ opencv/FindOpenCL.cmake
@@ -1,43 +1,42 @@
-set(ENV_ATISTREAMSDKROOT $ENV{ATISTREAMSDKROOT})
+set(ENV_ATISTREAMSDKROOT $ENV{AMDAPPSDKROOT})
 if(ENV_ATISTREAMSDKROOT)
-  find_path(
-    OPENCL_INCLUDE_DIR
-    NAMES CL/cl.h OpenCL/cl.h
-    PATHS $ENV{ATISTREAMSDKROOT}/include
-    NO_DEFAULT_PATH
-    )
+	  find_path(
+		OPENCL_INCLUDE_DIR
+		NAMES CL/cl.h OpenCL/cl.h
+		PATHS $ENV{AMDAPPSDKROOT}/include
+		NO_DEFAULT_PATH
+		)
 
-  if("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
-    if(CMAKE_SIZEOF_VOID_P EQUAL 4)
-      set(
-        OPENCL_LIB_SEARCH_PATH
-        ${OPENCL_LIB_SEARCH_PATH}
-        $ENV{ATISTREAMSDKROOT}/lib/x86
-        )
-    else(CMAKE_SIZEOF_VOID_P EQUAL 4)
-      set(
-        OPENCL_LIB_SEARCH_PATH
-        ${OPENCL_LIB_SEARCH_PATH}
-        $ENV{ATISTREAMSDKROOT}/lib/x86_64
-        )
-    endif(CMAKE_SIZEOF_VOID_P EQUAL 4)
-  endif("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
-  find_library(
-    OPENCL_LIBRARY
-    NAMES OpenCL
-    PATHS ${OPENCL_LIB_SEARCH_PATH}
-    NO_DEFAULT_PATH
-    )
+	if(CMAKE_SIZEOF_VOID_P EQUAL 4)
+	  set(
+		OPENCL_LIB_SEARCH_PATH
+		${OPENCL_LIB_SEARCH_PATH}
+		$ENV{AMDAPPSDKROOT}/lib/x86
+		)
+	else(CMAKE_SIZEOF_VOID_P EQUAL 4)
+	  set(
+		OPENCL_LIB_SEARCH_PATH
+		${OPENCL_LIB_SEARCH_PATH}
+		$ENV{AMDAPPSDKROOT}/lib/x86_64
+		)
+	endif(CMAKE_SIZEOF_VOID_P EQUAL 4)
+
+	find_library(
+		OPENCL_LIBRARY
+		NAMES OpenCL
+		PATHS ${OPENCL_LIB_SEARCH_PATH}
+		NO_DEFAULT_PATH
+		)
 else(ENV_ATISTREAMSDKROOT)
-  find_path(
-    OPENCL_INCLUDE_DIR
-    NAMES CL/cl.h OpenCL/cl.h
-    )
+	  find_path(
+		OPENCL_INCLUDE_DIR
+		NAMES CL/cl.h OpenCL/cl.h
+		)
 
-  find_library(
-    OPENCL_LIBRARY
-    NAMES OpenCL
-    )
+	  find_library(
+		OPENCL_LIBRARY
+		NAMES OpenCL
+		)
 endif(ENV_ATISTREAMSDKROOT)
 
 include(FindPackageHandleStandardArgs)
@@ -48,9 +47,9 @@ find_package_handle_standard_args(
   )
 
 if(OPENCL_FOUND)
-  set(OPENCL_LIBRARIES ${OPENCL_LIBRARY})
+  set(OPENCL_LIBRARY ${OPENCL_LIBRARY})
 else(OPENCL_FOUND)
-  set(OPENCL_LIBRARIES)
+  set(OPENCL_LIBRARY)
 endif(OPENCL_FOUND)
 
 mark_as_advanced(
