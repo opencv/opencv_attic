4979b2ad330957c247217f288aaff43ce8c4e18d
Vladislav Vinogradov <no@email>
2012-04-04 07:33:06 +0000

    merged with trunk r7817-7818
    git-svn-id: file://localhost/home/tolik/svnrepo/branches/2.4@7819 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- opencv_extra/gpu_demos_pack/demos/optical_flow/optical_flow.cpp
+++ opencv_extra/gpu_demos_pack/demos/optical_flow/optical_flow.cpp
@@ -17,14 +17,18 @@ using namespace cv::gpu;
 #define PARAM_METHOD "--method"
 #define PARAM_OFFSET "--offset"
 
-struct DrawData
-{
-    GlTexture tex;
-    GlArrays arr;
-    string method;
-    string total_fps;
-    string proc_fps;
-};
+//#define USE_OPENGL
+
+#ifdef USE_OPENGL
+    struct DrawData
+    {
+        GlTexture tex;
+        GlArrays arr;
+        string method;
+        string total_fps;
+        string proc_fps;
+    };
+#endif
 
 class App : public BaseApp
 {
@@ -49,7 +53,10 @@ protected:
 
 private:
     int the_same_video_offset;
+
+#ifdef USE_OPENGL
     DrawData draw_data;
+#endif
 };
 
 bool App::parseCmdArgs(int& i, int argc, const char* argv[])
@@ -119,33 +126,35 @@ void App::printHelp()
     BaseApp::printHelp();
 }
 
-void drawCallback(void* userdata)
-{
-    DrawData* data = static_cast<DrawData*>(userdata);
-
-    if (data->tex.empty() || data->arr.empty())
-        return;
-
-    static GlCamera camera;
-    static bool init_camera = true;
-
-    if (init_camera)
+#ifdef USE_OPENGL
+    void drawCallback(void* userdata)
     {
-        camera.setOrthoProjection(0.0, 1.0, 1.0, 0.0, 0.0, 1.0);
-        camera.lookAt(Point3d(0.0, 0.0, 1.0), Point3d(0.0, 0.0, 0.0), Point3d(0.0, 1.0, 0.0));
-        init_camera = false;
+        DrawData* data = static_cast<DrawData*>(userdata);
+
+        if (data->tex.empty() || data->arr.empty())
+            return;
+
+        static GlCamera camera;
+        static bool init_camera = true;
+
+        if (init_camera)
+        {
+            camera.setOrthoProjection(0.0, 1.0, 1.0, 0.0, 0.0, 1.0);
+            camera.lookAt(Point3d(0.0, 0.0, 1.0), Point3d(0.0, 0.0, 0.0), Point3d(0.0, 1.0, 0.0));
+            init_camera = false;
+        }
+
+        camera.setupProjectionMatrix();
+        camera.setupModelViewMatrix();
+
+        render(data->tex);
+        render(data->arr, RenderMode::TRIANGLES);
+
+        render(data->method, GlFont::get("Courier New", 24, GlFont::WEIGHT_BOLD), Scalar::all(255), Point2d(3.0, 0.0));
+        render(data->total_fps, GlFont::get("Courier New", 24, GlFont::WEIGHT_BOLD), Scalar::all(255), Point2d(3.0, 30.0));
+        render(data->proc_fps, GlFont::get("Courier New", 24, GlFont::WEIGHT_BOLD), Scalar::all(255), Point2d(3.0, 60.0));
     }
-
-    camera.setupProjectionMatrix();
-    camera.setupModelViewMatrix();
-
-    render(data->tex);
-    render(data->arr, RenderMode::TRIANGLES);
-
-    render(data->method, GlFont::get("Courier New", 24, GlFont::WEIGHT_BOLD), Scalar::all(255), Point2d(3.0, 0.0));
-    render(data->total_fps, GlFont::get("Courier New", 24, GlFont::WEIGHT_BOLD), Scalar::all(255), Point2d(3.0, 30.0));
-    render(data->proc_fps, GlFont::get("Courier New", 24, GlFont::WEIGHT_BOLD), Scalar::all(255), Point2d(3.0, 60.0));
-}
+#endif
 
 void download(const GpuMat& d_mat, vector<Point2f>& vec)
 {
@@ -200,6 +209,66 @@ void drawArrows(Mat& frame, const vector<Point2f>& prevPts, const vector<Point2f
     }
 }
 
+void drawArrowsDense(const Mat& u, const Mat& v, Mat& frame, Scalar line_color = Scalar(0, 0, 255))
+{
+    const int NEEDLE_MAP_SCALE = 16;
+    const int line_thickness = 1;
+    const float scale = 1.0f / (NEEDLE_MAP_SCALE * NEEDLE_MAP_SCALE);
+
+    static Mat u_sum;
+    static Mat v_sum;
+
+    integral(u, u_sum, CV_32F);
+    integral(v, v_sum, CV_32F);
+
+    for (int y = NEEDLE_MAP_SCALE / 2; y < u.rows- NEEDLE_MAP_SCALE / 2; y += NEEDLE_MAP_SCALE)
+    {
+        for (int x = NEEDLE_MAP_SCALE / 2; x < u.cols- NEEDLE_MAP_SCALE / 2; x += NEEDLE_MAP_SCALE)
+        {
+            float u_avg = u_sum.at<float>(y - NEEDLE_MAP_SCALE / 2, x - NEEDLE_MAP_SCALE / 2) + 
+                          u_sum.at<float>(y + NEEDLE_MAP_SCALE / 2, x + NEEDLE_MAP_SCALE / 2) -
+                          u_sum.at<float>(y + NEEDLE_MAP_SCALE / 2, x - NEEDLE_MAP_SCALE / 2) -
+                          u_sum.at<float>(y - NEEDLE_MAP_SCALE / 2, x + NEEDLE_MAP_SCALE / 2);
+
+            float v_avg = v_sum.at<float>(y - NEEDLE_MAP_SCALE / 2, x - NEEDLE_MAP_SCALE / 2) + 
+                          v_sum.at<float>(y + NEEDLE_MAP_SCALE / 2, x + NEEDLE_MAP_SCALE / 2) -
+                          v_sum.at<float>(y + NEEDLE_MAP_SCALE / 2, x - NEEDLE_MAP_SCALE / 2) -
+                          v_sum.at<float>(y - NEEDLE_MAP_SCALE / 2, x + NEEDLE_MAP_SCALE / 2);
+
+            u_avg *= scale;
+            v_avg *= scale;
+
+            Point p(x, y);
+            Point q(x + u_avg, y + v_avg);
+
+            double angle = atan2((double) p.y - q.y, (double) p.x - q.x);
+
+            double hypotenuse = sqrt( (double)(p.y - q.y)*(p.y - q.y) + (double)(p.x - q.x)*(p.x - q.x) );
+
+            if (hypotenuse < 1.0)
+                continue;
+
+            // Here we lengthen the arrow by a factor of three.
+            q.x = (int) (p.x - 3 * hypotenuse * cos(angle));
+            q.y = (int) (p.y - 3 * hypotenuse * sin(angle));
+
+            // Now we draw the main line of the arrow.
+            line(frame, p, q, line_color, line_thickness);
+
+            // Now draw the tips of the arrow. I do some scaling so that the
+            // tips look proportional to the main line of the arrow.
+
+            p.x = (int) (q.x + 9 * cos(angle + CV_PI / 4));
+            p.y = (int) (q.y + 9 * sin(angle + CV_PI / 4));
+            line(frame, p, q, line_color, line_thickness);
+
+            p.x = (int) (q.x + 9 * cos(angle - CV_PI / 4));
+            p.y = (int) (q.y + 9 * sin(angle - CV_PI / 4));
+            line(frame, p, q, line_color, line_thickness);
+        }
+    }
+}
+
 void App::process()
 {
     Ptr<PairFrameSource> source;
@@ -218,8 +287,21 @@ void App::process()
         source = PairFrameSource::get(sources[0], sources[1]);
     }
 
-    namedWindow("optical_flow_demo", WINDOW_OPENGL);
-    setGlDevice();
+#ifdef USE_OPENGL
+    bool useGl = false;
+
+    try
+    {
+        namedWindow("optical_flow_demo", WINDOW_OPENGL);
+        setGlDevice();
+        useGl = true;
+    }
+    catch (const cv::Exception&)
+    {
+        namedWindow("optical_flow_demo");
+        useGl = false;
+    }
+#endif
 
     Mat frame0, frame1, uv, u, v;
 
@@ -273,22 +355,47 @@ void App::process()
 
             double proc_fps = getTickFrequency() / (getTickCount() - proc_start);
 
-            createOpticalFlowNeedleMap(d_u, d_v, d_vertex, d_colors);
+#ifdef USE_OPENGL
+            if (useGl)
+            {
+                createOpticalFlowNeedleMap(d_u, d_v, d_vertex, d_colors);
 
-            stringstream total_fps_str;
-            total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
+                stringstream total_fps_str;
+                total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
 
-            stringstream proc_fps_str;
-            proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
+                stringstream proc_fps_str;
+                proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
 
-            draw_data.tex.copyFrom(d_frame0Gray32F);
-            draw_data.arr.setVertexArray(d_vertex);
-            draw_data.arr.setColorArray(d_colors, false);
-            draw_data.method = "Brox";
-            draw_data.total_fps = total_fps_str.str();
-            draw_data.proc_fps = proc_fps_str.str();
-            setOpenGlDrawCallback("optical_flow_demo", drawCallback, &draw_data);
-            updateWindow("optical_flow_demo");
+                draw_data.tex.copyFrom(d_frame0Gray32F);
+                draw_data.arr.setVertexArray(d_vertex);
+                draw_data.arr.setColorArray(d_colors, false);
+                draw_data.method = "Brox GPU";
+                draw_data.total_fps = total_fps_str.str();
+                draw_data.proc_fps = proc_fps_str.str();
+                setOpenGlDrawCallback("optical_flow_demo", drawCallback, &draw_data);
+                updateWindow("optical_flow_demo");
+            }
+            else
+#endif
+            {
+                Mat image = frame0.clone();
+                d_u.download(u);
+                d_v.download(v);
+
+                drawArrowsDense(u, v, image, Scalar(255, 0, 0));
+
+                stringstream total_fps_str; 
+                total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
+
+                stringstream proc_fps_str; 
+                proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
+
+                printText(image, "Brox GPU", 0);
+                printText(image, total_fps_str.str(), 1);
+                printText(image, proc_fps_str.str(), 2);
+
+                imshow("optical_flow_demo", image);
+            }
         }
         else if (method == LK_DENSE)
         {
@@ -301,22 +408,47 @@ void App::process()
 
             double proc_fps = getTickFrequency()  / (getTickCount() - proc_start);
 
-            createOpticalFlowNeedleMap(d_u, d_v, d_vertex, d_colors);
+#ifdef USE_OPENGL
+            if (useGl)
+            {
+                createOpticalFlowNeedleMap(d_u, d_v, d_vertex, d_colors);
 
-            stringstream total_fps_str;
-            total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
+                stringstream total_fps_str;
+                total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
 
-            stringstream proc_fps_str;
-            proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
+                stringstream proc_fps_str;
+                proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
 
-            draw_data.tex.copyFrom(d_frame0Gray);
-            draw_data.arr.setVertexArray(d_vertex);
-            draw_data.arr.setColorArray(d_colors, false);
-            draw_data.method = "PyrLK Dense";
-            draw_data.total_fps = total_fps_str.str();
-            draw_data.proc_fps = proc_fps_str.str();
-            setOpenGlDrawCallback("optical_flow_demo", drawCallback, &draw_data);
-            updateWindow("optical_flow_demo");
+                draw_data.tex.copyFrom(d_frame0Gray);
+                draw_data.arr.setVertexArray(d_vertex);
+                draw_data.arr.setColorArray(d_colors, false);
+                draw_data.method = "PyrLK Dense GPU";
+                draw_data.total_fps = total_fps_str.str();
+                draw_data.proc_fps = proc_fps_str.str();
+                setOpenGlDrawCallback("optical_flow_demo", drawCallback, &draw_data);
+                updateWindow("optical_flow_demo");
+            }
+            else
+#endif
+            {
+                Mat image = frame0.clone();
+                d_u.download(u);
+                d_v.download(v);
+
+                drawArrowsDense(u, v, image, Scalar(255, 0, 0));
+
+                stringstream total_fps_str; 
+                total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
+
+                stringstream proc_fps_str; 
+                proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
+
+                printText(image, "PyrLK Dense GPU", 0);
+                printText(image, total_fps_str.str(), 1);
+                printText(image, proc_fps_str.str(), 2);
+
+                imshow("optical_flow_demo", image);
+            }
         }
         else if (method == LK_SPARSE_GPU)
         {
@@ -387,22 +519,47 @@ void App::process()
             farneback_gpu(d_frame0Gray, d_frame1Gray, d_u, d_v);
             double proc_fps = getTickFrequency()  / (getTickCount() - proc_start);
 
-            createOpticalFlowNeedleMap(d_u, d_v, d_vertex, d_colors);
+#ifdef USE_OPENGL
+            if (useGl)
+            {
+                createOpticalFlowNeedleMap(d_u, d_v, d_vertex, d_colors);
 
-            stringstream total_fps_str; 
-            total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
+                stringstream total_fps_str;
+                total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
 
-            stringstream proc_fps_str; 
-            proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
+                stringstream proc_fps_str;
+                proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
 
-            draw_data.tex.copyFrom(d_frame0Gray);
-            draw_data.arr.setVertexArray(d_vertex);
-            draw_data.arr.setColorArray(d_colors, false);
-            draw_data.method = "Farneback GPU";
-            draw_data.total_fps = total_fps_str.str();
-            draw_data.proc_fps = proc_fps_str.str();
-            setOpenGlDrawCallback("optical_flow_demo", drawCallback, &draw_data);
-            updateWindow("optical_flow_demo");
+                draw_data.tex.copyFrom(d_frame0Gray);
+                draw_data.arr.setVertexArray(d_vertex);
+                draw_data.arr.setColorArray(d_colors, false);
+                draw_data.method = "Farneback GPU";
+                draw_data.total_fps = total_fps_str.str();
+                draw_data.proc_fps = proc_fps_str.str();
+                setOpenGlDrawCallback("optical_flow_demo", drawCallback, &draw_data);
+                updateWindow("optical_flow_demo");
+            }
+            else
+#endif
+            {
+                Mat image = frame0.clone();
+                d_u.download(u);
+                d_v.download(v);
+
+                drawArrowsDense(u, v, image, Scalar(255, 0, 0));
+
+                stringstream total_fps_str; 
+                total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
+
+                stringstream proc_fps_str; 
+                proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
+
+                printText(image, "Farneback GPU", 0);
+                printText(image, total_fps_str.str(), 1);
+                printText(image, proc_fps_str.str(), 2);
+
+                imshow("optical_flow_demo", image);
+            }
         }
         else if (method == FARNEBACK_CPU)
         {
@@ -416,27 +573,54 @@ void App::process()
                     farneback_gpu.numIters, farneback_gpu.polyN, farneback_gpu.polySigma, farneback_gpu.flags);
             double proc_fps = getTickFrequency()  / (getTickCount() - proc_start);
 
-            d_uv = GpuMat(uv);
-            GpuMat uv_planes[] = {d_u, d_v};
-            split(d_uv, uv_planes);
-            d_u = uv_planes[0]; 
-            d_v = uv_planes[1];
-            createOpticalFlowNeedleMap(d_u, d_v, d_vertex, d_colors);
+#ifdef USE_OPENGL
+            if (useGl)
+            {
+                d_uv.upload(uv);
+                GpuMat uv_planes[] = {d_u, d_v};
+                split(d_uv, uv_planes);
 
-            stringstream total_fps_str; 
-            total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
+                createOpticalFlowNeedleMap(d_u, d_v, d_vertex, d_colors);
 
-            stringstream proc_fps_str; 
-            proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
+                stringstream total_fps_str;
+                total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
 
-            draw_data.tex.copyFrom(d_frame0Gray);
-            draw_data.arr.setVertexArray(d_vertex);
-            draw_data.arr.setColorArray(d_colors, false);
-            draw_data.method = "Farneback CPU";
-            draw_data.total_fps = total_fps_str.str();
-            draw_data.proc_fps = proc_fps_str.str();
-            setOpenGlDrawCallback("optical_flow_demo", drawCallback, &draw_data);
-            updateWindow("optical_flow_demo");
+                stringstream proc_fps_str;
+                proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
+
+                draw_data.tex.copyFrom(d_frame0Gray);
+                draw_data.arr.setVertexArray(d_vertex);
+                draw_data.arr.setColorArray(d_colors, false);
+                draw_data.method = "Farneback CPU";
+                draw_data.total_fps = total_fps_str.str();
+                draw_data.proc_fps = proc_fps_str.str();
+                setOpenGlDrawCallback("optical_flow_demo", drawCallback, &draw_data);
+                updateWindow("optical_flow_demo");
+            }
+            else
+#endif
+            {
+                Mat uv_planes[] = {u, v};
+                split(uv, uv_planes);
+
+                Mat image = frame0.clone();
+                d_u.download(u);
+                d_v.download(v);
+
+                drawArrowsDense(u, v, image, Scalar(255, 0, 0));
+
+                stringstream total_fps_str; 
+                total_fps_str << "Total FPS : " << setprecision(4) << total_fps;
+
+                stringstream proc_fps_str; 
+                proc_fps_str << "Processing FPS : " << setprecision(4) << proc_fps;
+
+                printText(image, "Farneback CPU", 0);
+                printText(image, total_fps_str.str(), 1);
+                printText(image, proc_fps_str.str(), 2);
+
+                imshow("optical_flow_demo", image);
+            }
         }
 
         processKey(waitKey(10));
