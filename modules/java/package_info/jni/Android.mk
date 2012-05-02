6dd5979a724c93df90b31c6a0c31fbb41a6643d9
Alexander Smorkalov <no@email>
2012-05-02 09:51:27 +0000

    JNI interface for GetLibraryList() added.
    git-svn-id: file://localhost/home/tolik/svnrepo/branches/ocv_android_service@8286 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- modules/java/package_info/jni/Android.mk
+++ modules/java/package_info/jni/Android.mk
@@ -7,10 +7,11 @@ LOCAL_SRC_FILES := \
     src/info.c
 
 LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/include
+    $(LOCAL_PATH)/include \
+    $(TOP)/frameworks/base/core/jni
 
 LOCAL_PRELINK_MODULE := false
 
-LOCAL_MODULE := libOpenCVInfo
+LOCAL_MODULE := libopencvinfo
 
 include $(BUILD_SHARED_LIBRARY)
--- modules/java/package_info/jni/src/info.c
+++ modules/java/package_info/jni/src/info.c
@@ -1,4 +1,5 @@
 #include "info.h"
+#include <jni.h>
 
 #ifndef LIB_STRING
     #define LIB_STRING "libtbb.so;libopencv_java.so"
@@ -7,4 +8,11 @@
 const char* GetLibraryList()
 {
     return LIB_STRING;
+}
+
+JNIEXPORT jstring JNICALL Java_org_opencv_android_StaticHelper_GetLibraryList(JNIEnv* jenv, jclass clazz)
+{
+    jstring result = (*jenv)->NewStringUTF(jenv, LIB_STRING);
+    
+    return result;
 }
