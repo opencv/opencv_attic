108afc946ec0d583ff2c9a77f37ceb1da252cf02
Alexander Smorkalov <no@email>
2012-05-03 09:21:54 +0000

    Info library info fixed.
    git-svn-id: file://localhost/home/tolik/svnrepo/branches/ocv_android_service@8296 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- modules/java/src/java/android+StaticHelper.java
+++ modules/java/src/java/android+StaticHelper.java
@@ -6,84 +6,84 @@ import android.util.Log;
 
 public class StaticHelper {
 
-	public static int initOpenCV()
-	{
-		int result;
-		String libs = "";
-		
-		Log.d(TAG, "Trying to get library list");
-		
-		try
-		{
-			System.loadLibrary("opencvinfo");
-			libs = GetLibraryList(); 		
-		}
-		catch(Exception e)
-		{
-			Log.e(TAG, "OpenCV error: Cannot load info library for OpenCV");
-		}
+    public static int initOpenCV()
+    {
+        int result;
+        String libs = "";
 
-		Log.d(TAG, "Library list: \"" + libs + "\"");
-		Log.d(TAG, "First attempt to load libs");
-		if (initOpenCVLibs(libs))
-		{
-			Log.d(TAG, "First attempt to load libs is OK");
-			result = OpenCVLoader.Success;
-		}
-		else
-		{
-			Log.d(TAG, "First attempt to load libs fails");
-			result = OpenCVLoader.InitFailed;
-		}
-				
-		return result;
-	}
-	
-	private static boolean loadLibrary(String Name)
-	{
-		boolean result = true;
-		
-		Log.d(TAG, "Trying to load library " + Name);
-		try
-		{
-			System.loadLibrary(Name);
-			Log.d(TAG, "OpenCV libs init was ok!");
-		}
-		catch(Exception e)
-		{
-			Log.d(TAG, "Cannot load library \"" + Name + "\"");
-			e.printStackTrace();
-			result &= false;
-		}
-		
-		return result;
-	}
-	
-	private static boolean initOpenCVLibs(String Libs)
-	{
-		Log.d(TAG, "Trying to init OpenCV libs");
+        Log.d(TAG, "Trying to get library list");
 
-		boolean result = true;
-		
-		if ((null != Libs) && (Libs.length() != 0))
-		{
-			Log.d(TAG, "Trying to load libs by dependency list");
-			StringTokenizer splitter = new StringTokenizer(Libs, ";");
-			while(splitter.hasMoreTokens())
-			{
-				result &= loadLibrary(splitter.nextToken());
-			}
-		}
-		else
-		{
-			// If dependencies list is not defined or empty
-			result &= loadLibrary("libopencv_java.so");
-		}
-		
-		return result;
-	}
-	
-	private static final String TAG = "OpenCV/StaticHelper";
-	
-	private static native String GetLibraryList();
+        try
+        {
+        	System.loadLibrary("opencvinfo");
+            libs = GetLibraryList();
+        }
+        catch(UnsatisfiedLinkError e)
+        {
+            Log.e(TAG, "OpenCV error: Cannot load info library for OpenCV");
+        }
+
+        Log.d(TAG, "Library list: \"" + libs + "\"");
+        Log.d(TAG, "First attempt to load libs");
+        if (initOpenCVLibs(libs))
+        {
+            Log.d(TAG, "First attempt to load libs is OK");
+            result = OpenCVLoader.Success;
+        }
+        else
+        {
+            Log.d(TAG, "First attempt to load libs fails");
+            result = OpenCVLoader.InitFailed;
+        }
+
+        return result;
+    }
+
+    private static boolean loadLibrary(String Name)
+    {
+        boolean result = true;
+
+        Log.d(TAG, "Trying to load library " + Name);
+        try
+        {
+            System.loadLibrary(Name);
+            Log.d(TAG, "OpenCV libs init was ok!");
+        }
+        catch(Exception e)
+        {
+            Log.d(TAG, "Cannot load library \"" + Name + "\"");
+            e.printStackTrace();
+            result &= false;
+        }
+
+        return result;
+    }
+
+    private static boolean initOpenCVLibs(String Libs)
+    {
+        Log.d(TAG, "Trying to init OpenCV libs");
+
+        boolean result = true;
+
+        if ((null != Libs) && (Libs.length() != 0))
+        {
+            Log.d(TAG, "Trying to load libs by dependency list");
+            StringTokenizer splitter = new StringTokenizer(Libs, ";");
+            while(splitter.hasMoreTokens())
+            {
+                result &= loadLibrary(splitter.nextToken());
+            }
+        }
+        else
+        {
+            // If dependencies list is not defined or empty
+            result &= loadLibrary("opencv_java");
+        }
+
+        return result;
+    }
+
+    private static final String TAG = "OpenCV/StaticHelper";
+
+    private static native String GetLibraryList();
 }
