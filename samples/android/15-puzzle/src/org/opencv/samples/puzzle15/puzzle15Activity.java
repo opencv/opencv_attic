d1a6e0a75bd59a066b0f498deee0757d575e6ebd
Alexander Smorkalov <no@email>
2012-06-08 13:58:31 +0000

    Face detection sample was ported to using OpenCV Manager
    git-svn-id: file://localhost/home/tolik/svnrepo/branches/ocv_android_service@8605 c5418bc3-7546-4aae-8bad-fa6f2f3e53e6

--- samples/android/15-puzzle/src/org/opencv/samples/puzzle15/puzzle15Activity.java
+++ samples/android/15-puzzle/src/org/opencv/samples/puzzle15/puzzle15Activity.java
@@ -97,7 +97,7 @@ public class puzzle15Activity extends Activity
         if (!OpenCVLoader.initAsync(OpenCVLoader.OPEN_CV_VERSION_2_4_0, this, mOpenCVCallBack))
         {
         	Log.e(TAG, "Cannot connect to OpenCVEngine");
-        	//finish();
+        	finish();
         }
     }
 
